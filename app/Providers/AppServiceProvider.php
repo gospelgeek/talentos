808422109e092bffd02b28dea7a097774da4a4fa
juliantello <yilver.tello@correounivalle.edu.co>
2022-02-09 14:05:39 -0500

    errores

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,10 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->bind('path.public',function(){
-            return'/home/todosytodasaestu/public_html';
-
-        });
+        //
     }
 
     /**

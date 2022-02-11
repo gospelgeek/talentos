7e8ee46ba9f3ad924196c6e201882b893f6444f8
juliantello <95248642+juliantello@users.noreply.github.com>
2022-02-11 16:39:07 -0500

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="shortcut icon" href="{{asset('img/logo.jpg')}}">
+    <link rel="shortcut icon" href="{{asset('img/logo.jpeg')}}">
 
     <title>PNA</title>
 

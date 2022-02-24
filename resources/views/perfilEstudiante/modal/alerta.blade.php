ebd49171c57a1c1cecfd207c4d03a2531a5a98a2
juliantello <95248642+juliantello@users.noreply.github.com>
2022-02-23 21:09:05 -0500

    Update alerta.blade.php

--- resources/views/perfilEstudiante/modal/alerta.blade.php
+++ resources/views/perfilEstudiante/modal/alerta.blade.php
@@ -1,5 +1,5 @@
 <div class="modal fade" id="modal_alerta">
-  <div class="modal-dialog modal-lg">
+  <div class="modal-dialog modal-sm">
       <div class="modal-content">
       <div class="modal-header">
          <h5 class="modal-title pull-center" style="justify-content: center">

2f650d944df538bff79da6c7094fdd0c02e363e4
juliantello <yilver.tello@correounivalle.edu.co>
2022-03-01 14:31:38 -0500

    mejoras asistencias

--- resources/views/perfilEstudiante/Asistencias/sesiones.blade.php
+++ resources/views/perfilEstudiante/Asistencias/sesiones.blade.php
@@ -1,14 +1,16 @@
 @extends('layouts.dashboard')
 
-@section('title', 'Sesiones')
+@section('title', $name->name ." ". $grupo->name)
 @section('content')
 <script id="json" type="text" src="/students.json"></script>
 <script id="asisten" type="text" src="/asistencias.json"></script>
 <h1 id="num_sesiones" style="text-align: center;">{{$name->name}}<br>{{$grupo->name}}<br> Total Sesiones </h1>
 <input type="hidden" id="code_curse" data-id="{{$grupo->id}}" data-name="{{ $name->name}}" data-cohort="{{ $grupo->cohort->id}}" data-group="{{$grupo->name}}" data-courseid="{{$name->id}}">
+<input type="hidden" id="moodle" name="quotation" data-datos="{{json_encode($id_moole,TRUE)}}">
 <div class="table-responsive" id="datos">
 
 </div>
+<a href="{{route('asistencias.grupos',$name->id)}}" class="fa fa-arrow-left">Regresar</a>
 {!!Form::open(['id'=>'form-edit','route'=>['asistencias.asignatura',$name->id,$grupo->id,':SESSIONID'], 'method'=>'GET'])!!}
 {!!Form::close()!!}
 @push('scripts')

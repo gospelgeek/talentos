e6f9ba1bd333e8c83ab07a9cd68f759315d851d2
juliantello <95248642+juliantello@users.noreply.github.com>
2022-03-03 14:41:13 -0500

    Update perfilEstudiante.php

--- app/perfilEstudiante.php
+++ app/perfilEstudiante.php
@@ -44,62 +44,6 @@ class perfilEstudiante extends Model
 
     protected $dates = ['delete_at'];
 
-    public static function consultaPrincipal(){
-
-        $perfilEstudiantes= DB::select("SELECT student_profile.id as idstudiante, student_profile.*,socioeconomic_data.id as idtabla, socioeconomic_data.id_student as idstudent, socioeconomic_data.id_civil_status as estadocivil, socioeconomic_data.id_ethnicity as etnia, YEAR(CURDATE())-YEAR(student_profile.birth_date) + IF(DATE_FORMAT(CURDATE(),'%m-%d') > DATE_FORMAT(student_profile.birth_date,'%m-%d'), 0 , -1 ) as edad, 
-            (SELECT document_type.name FROm document_type WHERE document_type.id = student_profile.id_document_type) as tipodocumento,
-            (SELECT birth_departaments.name FROM birth_departaments WHERE student_profile.id_birth_department = birth_departaments.id) as departamentoN,
-            (SELECT birth_city.name FROM birth_city WHERE student_profile.id_birth_city = birth_city.id) as ciudadN,
-            (SELECT comune.name FROM comune WHERE student_profile.id_commune = comune.id) as comuna,
-            (SELECT neighborhood.name FROM neighborhood WHERE student_profile.id_neighborhood = neighborhood.id) as barrio,
-            (SELECT gender.name FROM gender WHERE gender.id = student_profile.id_gender) as genero,
-            (SELECT tutor.name FROM tutor WHERE tutor.id = student_profile.id_tutor) as tutor,
-            (SELECT conditions.name FROM conditions WHERE conditions.id = student_profile.id_state) as estado,
-            (SELECT civil_statuses.name FROM civil_statuses WHERE socioeconomic_data.id_civil_status = civil_statuses.id) as nombreEstadocivil,
-            (SELECT ethnicities.name FROM ethnicities WHERE socioeconomic_data.id_ethnicity = ethnicities.id) as nombreEtnia,
-            (SELECT student_groups.id_group FROM student_groups WHERE student_groups.id_student = student_profile.id) as grupoid,
-            (SELECT groups.name FROM groups WHERE student_groups.id_group = groups.id) as namegrupo,
-            (SELECT cohorts.name FROM cohorts WHERE groups.id_cohort = cohorts.id) as cohorte
-            FROM student_profile, socioeconomic_data, student_groups, groups
-            WHERE student_profile.id = socioeconomic_data.id_student 
-            AND student_groups.id_student = student_profile.id 
-            AND student_groups.id_group = groups.id
-        ");
-
-            if ($perfilEstudiantes != null) {
-                return $perfilEstudiantes;
-            }else{
-                return null;
-            }
-    }
-
-    public function static consultaMayoriaEdad(){
-        
-        $mayoriaedad = DB::select("SELECT student_profile.id, student_profile.*, YEAR(CURDATE())-YEAR(student_profile.birth_date) + IF(DATE_FORMAT(CURDATE(),'%m-%d') > DATE_FORMAT(student_profile.birth_date,'%m-%d'), 0 , -1 ) as edad, 
-            (SELECT student_groups.id_group FROM student_groups WHERE student_groups.id_student = student_profile.id) as grupoid,
-            (SELECT groups.name FROM groups WHERE student_groups.id_group = groups.id) as namegrupo,
-            (SELECT cohorts.name FROM cohorts WHERE groups.id_cohort = cohorts.id) as cohorte,
-            (SELECT birth_departaments.name FROM birth_departaments WHERE student_profile.id_birth_department = birth_departaments.id) as departamentoN,
-            (SELECT birth_city.name FROM birth_city WHERE student_profile.id_birth_city = birth_city.id) as ciudadN,
-            (SELECT comune.name FROM comune WHERE student_profile.id_commune = comune.id) as comuna,
-            (SELECT neighborhood.name FROM neighborhood WHERE student_profile.id_neighborhood = neighborhood.id) as barrio
-            FROM student_profile, socioeconomic_data, student_groups, groups
-            WHERE student_profile.id = socioeconomic_data.id_student 
-            AND student_groups.id_student = student_profile.id 
-            AND student_groups.id_group = groups.id
-            AND YEAR(CURDATE())-YEAR(student_profile.birth_date) + IF(DATE_FORMAT(CURDATE(),'%m-%d') > DATE_FORMAT(student_profile.birth_date,'%m-%d'), 0 , -1 ) = 18
-            AND MONTH(birth_date) >= 02 AND MONTH(birth_date) <= MONTH(NOW())
-        ");
-
-        if ($mayoriaedad != null) {
-            
-            return $mayoriaedad;
-        }else{
-            return null;
-        }
-    }
-
-
     //RELACIONES UNO A UNO POR DEBAJO
     /**
      * Relacion con los  datos que se tiene de student_profile  

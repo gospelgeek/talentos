cf904e5b5b965286549bada1fb7792312f1c6f5d
juliantello <95248642+juliantello@users.noreply.github.com>
2022-03-10 18:03:46 -0500

    Update filtroestudiantes.js

--- public/js/filtroestudiantes.js
+++ public/js/filtroestudiantes.js
@@ -409,5 +409,31 @@ $(".accordion-titulo-4").click(function(e){
 
 });
 
+$(".accordion-titulo-6").click(function(e){
+           
+        e.preventDefault();
+    
+        var contenido=$(this).next(".accordion-content-6");
+ 
 
+        if(contenido.css("display")=="none"){ //open        
+          contenido.slideDown(250);         
+          $(this).addClass("open");
+          $("#contenido-1").hide();
+          $("#titulo-1").removeClass("open");
+          $("#contenido-2").hide();
+          $("#titulo-2").removeClass("open");
+          $("#contenido-3").hide();
+          $("#titulo-3").removeClass("open"); 
+          $("#contenido-4").hide();
+          $("#titulo-4").removeClass("open");  
+        }
+        else{ //close
+
+          contenido.slideUp(250);
+          $(this).removeClass("open");
+          
+            
+        }
+});
 

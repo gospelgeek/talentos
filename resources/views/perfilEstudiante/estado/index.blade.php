9367857a59e63d81af78ab11804ffc0638147843
juliantello <95248642+juliantello@users.noreply.github.com>
2022-03-10 17:33:20 -0500

    Add files via upload

---
+++ resources/views/perfilEstudiante/estado/index.blade.php
@@ -0,0 +1,58 @@
+@extends('layouts.dashboard')
+
+@section('title', 'Estudiantes Estados')
+@section('content')
+@include('../alerts.success')
+@include('../alerts.request')
+
+<div class="container-fluid">
+<h1 style="text-align:center;">ESTADO ESTUDIANTES</h1>
+    <div class="card">         
+    <div class="card-body">    
+    <div class="table-responsive">
+     <table id="example1" class=" table table-bordered table-striped">
+        <thead >
+            <tr >
+                <td>Nombres</td>
+                <td>Apellidos</td>
+                <td>Nº documento</td>
+                <td>Grupo</td>
+                <td>Cohorte</td>
+                <td>Estado</td>
+                <td>Acciones</td>
+            </tr>
+        </thead> 
+
+        <tbody>
+            @foreach ($verDatosPerfil  as $data)
+                <tr data-id="{{$data->id}}">
+                                        <td>{{ $data->name}}</td>
+                                        <td>{{ $data->lastname }}</td>
+                                        <td>{{ $data->document_number }}</td>
+                                        <td>{{ $data->studentGroup->group->name}}</td>
+                                        <td>{{ $data->studentGroup->group->cohort->name}}</td>
+                                        <td>{{ $data->condition->name}}</td>
+                                        <td >
+
+                                        @if(auth()->user()->rol_id == 4 || auth()->user()->rol_id == 1 || auth()->user()->rol_id == 2)   
+                                 
+                                                {!!link_to('#',$title = 'Cambiar Estado', $attributes = ['class'=>'btn  fa fa-pencil-square-o editar_estado',$secure = null])!!}
+                                        </td>
+                                        @csrf
+                </tr>
+                @endif
+            @endforeach    
+        </tbody>
+
+      </table>
+      </div>
+    </div>
+    </div>
+</div>
+{!!Form::open(['id'=>'form-edit','route'=>['estudiantes.estado_edit',':ESTADO_ID'], 'method'=>'GET'])!!}
+{!!Form::close()!!}
+@include('perfilEstudiante.estado.modal.edit_estado')
+@push('scripts')
+{!!Html::script('/js/estado.js')!!}
+@endpush
+@endsection

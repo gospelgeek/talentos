3663af3cdc986dbabee11bfe0a22ad4b57e9017b
StevenDavila <72047032+StevenDavila@users.noreply.github.com>
2022-03-23 17:21:17 -0500

    Update ReporteExport.php

--- app/Exports/ReporteExport.php
+++ app/Exports/ReporteExport.php
@@ -3,8 +3,9 @@
 namespace App\Exports;
 
 use Maatwebsite\Excel\Concerns\FromArray;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class ReporteExport implements FromArray
+class ReporteExport implements FromArray, WithHeadings
 {
     protected $invoices;
 
@@ -16,5 +17,14 @@ class ReporteExport implements FromArray
     public function array(): array
     {
         return $this->invoices;
+    }
+
+    public function headings(): array
+    {
+        return [
+            ['Course_id','Shortname','Total Sesiones','Total Asistencias','Promedio Asistencias'
+            ]
+            
+        ];
     }
 }

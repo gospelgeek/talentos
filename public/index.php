f9ae4f8a3a24151136d8156965284d91cfe27766
juliantello <95248642+juliantello@users.noreply.github.com>
2022-03-28 21:25:54 -0500

    Update index.php

--- public/index.php
+++ public/index.php
@@ -26,7 +26,7 @@ define('LARAVEL_START', microtime(true));
 
 
 
-require __DIR__.'/../PNA/vendor/autoload.php';
+require __DIR__.'/../PNApruebas/vendor/autoload.php';
 
 
 
@@ -50,7 +50,7 @@ require __DIR__.'/../PNA/vendor/autoload.php';
 
 
 
-$app = require_once __DIR__.'/../PNA/bootstrap/app.php';
+$app = require_once __DIR__.'/../PNApruebas/bootstrap/app.php';
 
 
 

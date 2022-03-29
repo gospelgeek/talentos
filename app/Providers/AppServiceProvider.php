9eafcc0f1097cb66b9867f342450ed579127e632
juliantello <95248642+juliantello@users.noreply.github.com>
2022-03-28 21:26:32 -0500

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
     {
 
             $this->app->bind('path.public',function(){
-            return'/home/todosytodasaestu/public_html';
+            return'/home/todosytodasaestu/pruebas.todosytodasaestudiar.org';
             });
 
     }

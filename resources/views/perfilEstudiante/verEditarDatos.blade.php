ef5874289d6e10f20eee57c4c975f88738fe5143
juliantello <95248642+juliantello@users.noreply.github.com>
2022-03-31 12:21:11 -0500

    Update verEditarDatos.blade.php

--- resources/views/perfilEstudiante/verEditarDatos.blade.php
+++ resources/views/perfilEstudiante/verEditarDatos.blade.php
@@ -221,7 +221,7 @@
 			<div class="form-group">
     			<div class="row">
             		<div class="col-xs-2 col-md-2">
-            			<p style="text-align: right"><label for="id_neighborhood">Barrio Residencia *</label></p>
+            			<p style="text-align: right"><label for="id_neighborhood">Barrio de residencia *</label></p>
             		</div>
 					<div class="col-xs-2 col-md-2">
 						<div class="row">

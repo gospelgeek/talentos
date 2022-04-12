aecc11215bac8ab3d8c2df22eaa80618b476a488
juliantello <95248642+juliantello@users.noreply.github.com>
2022-04-11 20:15:33 -0500

    Create alerta.blade.php

---
+++ resources/views/perfilEstudiante/almuerzos/modal/alerta.blade.php
@@ -0,0 +1,40 @@
+<div class="modal fade" id="modal_alerta_almuerzos" data-backdrop="static" data-keyboard="false" tabindex="-1">
+  <div class="modal-dialog modal-md">
+      <div class="modal-content">
+      <div class="modal-header">
+         <h5 class="modal-title pull-center" style="justify-content: center">
+          <strong>MENSAJE DE ALERTA</strong> 
+          </h5>
+        
+      </div>
+      <div id="msj-error-edit" class="alert alert-danger alert-dismissible" role="alert" style="display:none">
+        <strong id="msj"></strong>
+      </div>  
+      <div class="modal-body">
+        <div class="container-fluid">
+          <div class="row">
+          
+            <span id="dtos">
+             ¿Está seguro que desea eliminar el registro de almuerzos del &nbsp;<br><b id="fecha_almuerzo"></b>?
+            </span>    
+          </div>           
+        </div>
+      
+          <div class="modal-footer">
+            <div class="container-fluid">
+              <div class="row">
+                <div class="col-xs-6 col-sm-6">
+                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                  <a id="id_registro" class="btn bg-primary" style="width:47.2px;" onclick="boton_delete_registro_almuerzos(this)">SI</a> 
+                </div>
+                <div class="col-xs-6 col-sm-6 ">
+                  {!!link_to('#',$title = 'NO', $attributes = ['class'=>'btn bg-danger  elevation-3 cerrar_modal'],$secure = null)!!}                        
+                  {!!Form::close()!!}
+                </div>
+              </div>
+            </div>
+          </div>
+      </div>
+    </div>
+  </div>
+</div>

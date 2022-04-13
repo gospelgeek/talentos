ac927ccf5e67bd558507fd151f9e344c26b9366b
juliantello <95248642+juliantello@users.noreply.github.com>
2022-04-12 19:27:07 -0500

    Update dashboard.blade.php

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -205,14 +205,21 @@ scratch. This page gets rid of all links and provides the needed markup only.
               </a>
               <div class="dropdown-menu" aria-labelledby="navbarDropdown1">
                 <a class="dropdown-item" href="{{route('estudiante')}}">Reporte General</a>
-                <a class="dropdown-item" href="{{route('estudiantes.estado')}}">Estado Estudiantes</a>
-                <a class="dropdown-item" href="{{route('estudiantes.estado')}}">Estado Estudiantes</a>
-                <a class="dropdown-item" href="{{route('estudiantes_mayoria_edad')}}">Mayoria de edad</a>
-                <a  href="{{route('formalizacion')}}" class="dropdown-item">Formalizacion</a> 
+                <a class="dropdown-item" href="{{route('estudiantes.estado')}}">Estado Estudiantes</a> 
                 <a href="{{route('graficas')}}" class="dropdown-item"><i></i>Estadisticas Graficas</a>
                </div>
               
           </li>
+          <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" id="navbarDropdown1" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Administrativo
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown1">
+            <a href="{{route('almuerzos_estudiantes')}}" class="dropdown-item">Almuerzos</a>
+            <a href="{{route('formalizacion')}}" class="dropdown-item">Formalizacion</a>
+            <a class="dropdown-item" href="{{route('estudiantes_mayoria_edad')}}">Mayoria de edad</a>
+          </div>
+        </li>
           <a href="{{route('socioeducativo')}}" class="dropdown-item dropdown-footer"><i></i>Socioeducativo</a>
           <li class="nav-item dropdown">
             <a id="dropdownSubMenu1" href="{{route('asistencias')}}" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link dropdown-toggle">Asistencias</a>
@@ -233,10 +240,18 @@ scratch. This page gets rid of all links and provides the needed markup only.
                           <div class="dropdown-menu" aria-labelledby="navbarDropdown1">
                             <a class="dropdown-item" href="{{route('estudiante')}}">Reporte General</a>
                             <a class="dropdown-item" href="{{route('estudiantes.estado')}}">Estado Estudiantes</a>
-                            <a class="dropdown-item" href="{{route('estudiantes_mayoria_edad')}}">Mayoria de edad</a>
-                            <a  href="{{route('formalizacion')}}" class="dropdown-item">Formalizacion</a>
                             <a href="{{route('graficas')}}" class="dropdown-item"><i></i>Estadisticas Graficas</a>
                           </div>
+                      </li>
+                      <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" id="navbarDropdown1" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                          Administrativo
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown1">
+                          <a href="{{route('almuerzos_estudiantes')}}" class="dropdown-item">Almuerzos</a>
+                          <a class="dropdown-item" href="{{route('estudiantes_mayoria_edad')}}">Mayoria de edad</a>
+                          <a  href="{{route('formalizacion')}}" class="dropdown-item">Formalizacion</a>
+                        </div>
                       </li>
                       
                         <a href="{{route('asignaturas')}}" class="dropdown-item dropdown-footer"><i></i>Asignaturas</a>

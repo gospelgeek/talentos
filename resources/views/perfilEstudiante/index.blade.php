ce63cf36764bf169ed27fff8536bf1cc94b95717
juliantello <95248642+juliantello@users.noreply.github.com>
2022-04-19 14:39:35 -0500

    Update index.blade.php

--- resources/views/perfilEstudiante/index.blade.php
+++ resources/views/perfilEstudiante/index.blade.php
@@ -24,7 +24,7 @@
     <h1 style="text-align:center;">ESTUDIANTES</h1>
     <div class="card">         
     <div class="card-body">
-        @if(auth()->user()->rol_id == 4 || auth()->user()->rol_id == 1) 
+        @if(auth()->user()->rol_id == 4 || auth()->user()->rol_id == 1 || auth()->user()->rol_id == 5) 
         <div class="btn-group">
             <div class="col-xs-6 col-md-5 col-sm-3">
                     <a class="btn btn-primary btn-sm mt-3 mb-3 float-left" href="{{route('crear_estudiante')}}">Crear Perfil</a>            
@@ -44,10 +44,11 @@
 
         </div>
         <div class="inactivos_student">
-            <center>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<label>INACTIVOS</label>&nbsp;<input type="checkbox" name="filtro" value="ACTIVO" id="inactivos">
+            <center>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<label>INACTIVOS</label>&nbsp;<input type="checkbox" name="filtro" value="ACTIVO" id="inactivos">
             
             </center>  
         </div>
+        
         
         @endif
         
@@ -61,29 +62,13 @@
             <tr>
                 <td>Nombres</td>
                 <td>Apellidos</td>
-                <td>Tipo Documento</td>
-                <td>Fecha expedicion documento</td>
-                <td>Nº Documento</td>
-                <td>Codigo</td>
+                <td>Numero documento</td>
+                <td>Codigo estudiante</td>
                 <td>Email</td>
                 <td>Telefono</td>
                 <td id="group">Grupo</td>
                 <td>Cohorte</td>
-                <td>Fecha nacimiento</td>
-                <td>Edad</td>
-                <td>Dpto. Nacimiento</td>
-                <td>Ciudad Nacimiento</td>
-                <td>Sexo</td>
-                <td>Genero</td>
-                <td>Direcion</td>
-                <td>Comuna</td>
-                <td>Barrio</td>
-                <td>Tel. Alternativo</td>
-                <td>Tutor</td>
                 <td>Estado</td>
-                <td>Estado Civil</td>
-                <td>Etnia</td>
-                <td>Institucion</td>
                 <td id="botons" width="15%">Acciones</td>
             </tr>
         </thead>       
@@ -98,13 +83,7 @@
     <!-- Page specific script -->
 <script>
 
-/*$('input[type="checkbox"]').change(function (){
-    var ver = $("input[name=filtro]:checked").val();
-});*/
-     
-       
-
-        var table = $("#example1").DataTable({
+       var table = $("#example1").DataTable({
             
             "ajax":{
                 "method":"GET",
@@ -114,30 +93,13 @@
             "columns": [
                 {data: 'name'},
                 {data: 'lastname'},
-                {data: 'tipodocumento', visible: false},
-                {data: 'document_expedition_date', visible: false},
                 {data: 'document_number'},
                 {data: 'student_code'},
                 {data: 'email'},
                 {data: 'cellphone'},
-                {data: 'namegrupo'},
+                {data: 'grupo'},
                 {data: 'cohorte'},
-                {data: 'birth_date', visible: false},
-                {data: 'edad', visible: false},
-                {data: 'departamentoN', visible: false},
-                {data: 'ciudadN', visible: false},
-                {data: 'sex', visible: false},
-                {data: 'genero', visible: false},
-                {data: 'direction', visible: false},
-                {data: 'comuna', visible:false},
-                {data: 'barrio', visible: false},
-                {data: 'phone', visible: false},
-                {data: 'tutor', visible: false},
-                {data: 'estado', visible: false},
-                {data: 'nombreEstadocivil', visible: false},
-                {data: 'nombreEtnia', visible: false},
-                {data: 'colegio', visible: false},
-
+                {data: 'estado.name', visible:false},
                 {data: null, render:function(data, type, row, meta){
                     var rol = document.getElementById('roles').value;
                     var mstr;
@@ -177,10 +139,12 @@
                 
             ]
         });
+
         document.getElementById('linea_1').checked = true;
         document.getElementById('linea_2').checked = true;
         document.getElementById('linea_3').checked = true;
         
+
         $('.filtroCohortes').on('change', function() {
            
             var checkLinea1 = $('#linea_1').is(":checked");
@@ -190,14 +154,18 @@
 
             if (!checkLinea1) {
                     if(checkLinea2 && checkLinea3){
+                        //filtro por columna excepto el valor de del id del checbox indicado(linea_1)
                         var filtro = $('input:checkbox[id="linea_1"]').map(function() {
                             return this.value;
                         }).get().join('|');
-                        table.column(9).search(filtro ? '^((?!' + filtro + ').*)$' : '', true, false, false).draw(false);
+                        table.column(7).search(filtro ? '^((?!' + filtro + ').*)$' : '', true, false, false).draw(false);
+                        //
                     }else if (checkLinea2) {
-                        table.columns(9).search('LINEA 2');        
+                        //filtros basicos por columna con un solo valor
+                        table.columns(7).search('LINEA 2'); 
+                        //       
                     }else if (checkLinea3) {
-                        table.columns(9).search('LINEA 3');
+                        table.columns(7).search('LINEA 3');
                     }
                     table.draw();
                         
@@ -208,11 +176,11 @@
                         var filtro = $('input:checkbox[id="linea_2"]').map(function() {
                             return this.value;
                         }).get().join('|');
-                        table.column(9).search(filtro ? '^((?!' + filtro + ').*)$' : '', true, false, false).draw(false);
+                        table.column(7).search(filtro ? '^((?!' + filtro + ').*)$' : '', true, false, false).draw(false);
                     }else if(checkLinea1){
-                        table.columns(9).search('LINEA 1');
+                        table.columns(7).search('LINEA 1');
                     }else if(checkLinea3){
-                        table.columns(9).search('LINEA 3');
+                        table.columns(7).search('LINEA 3');
                     }
                     table.draw();
                 }
@@ -223,24 +191,29 @@
                         var filtro = $('input:checkbox[id="linea_3"]').map(function() {
                             return this.value;
                         }).get().join('|');
-                        table.column(9).search(filtro ? '^((?!' + filtro + ').*)$' : '', true, false, false).draw(false);
+                        table.column(7).search(filtro ? '^((?!' + filtro + ').*)$' : '', true, false, false).draw(false);
                     }else if(checkLinea1){
-                        table.columns(9).search('LINEA 1');
+                        table.columns(7).search('LINEA 1');
                     }else if(checkLinea2){
-                        table.columns(9).search('LINEA 2');
+                        table.columns(7).search('LINEA 2');
                     }
                     table.draw();
                 }
 
                 if (checkLinea1 && checkLinea2 && checkLinea3) {
-                    
+                    //filtro por columna con varios valores segun el name de los checbox y su valor correspondiente
                     var offices = $('input:checkbox[name="check"]:checked').map(function() {
                         return this.value;
                     }).get().join('|');
-                    table.column(9).search(offices, true, false, false).draw(false);
+                    table.column(7).search(offices, true, false, false).draw(false);
+                    //
                 }
+                    
+                
+                            
+
         });
-        
+
         $('.inactivos_student').on('change', function() {
             //alert('checkLinea1');
             var inctvs = $('#inactivos').is(":checked");
@@ -250,7 +223,7 @@
                 var filtro = $('input:checkbox[id="inactivos"]').map(function() {
                     return this.value;
                 }).get().join('|');
-                table.column(21).search(filtro ? '^((?!' + filtro + ').*)$' : '', true, false, false).draw(false);
+                table.column(8).search(filtro ? '^((?!' + filtro + ').*)$' : '', true, false, false).draw(false);
                 //
             }
 
@@ -259,10 +232,11 @@
                 var offices = $('input:checkbox[name="filtro"]:checked').map(function() {
                     return this.value;
                 }).get().join('|');
-                table.column(21).search(offices, true, false, false).draw(false);
+                table.column(8).search(offices, true, false, false).draw(false);
                 //
             }
         });
+    
 
                 
 </script>

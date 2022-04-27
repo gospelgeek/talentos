660278c5d59f911500b3d2ea5c51330e33ddb510
StevenDavila <72047032+StevenDavila@users.noreply.github.com>
2022-04-27 18:54:29 -0500

    Update perfilEstudianteController.php

--- app/Http/Controllers/perfilEstudianteController.php
+++ app/Http/Controllers/perfilEstudianteController.php
@@ -2317,7 +2317,7 @@ class perfilEstudianteController extends Controller
     public function export(){
 
         
-        $estudiantes = perfilEstudiante::select('id', 'college', 'registration_date', 'email', 'name', 'lastname', 'id_document_type', 'document_number', 'document_expedition_date', 'landline', 'cellphone', 'phone', 'id_birth_city', 'direction', 'id_neighborhood', 'id_gender', 'id_tutor', 'birth_date')->get();
+        $estudiantes = perfilEstudiante::select('id', 'college', 'registration_date', 'email', 'name', 'lastname', 'id_document_type', 'document_number', 'document_expedition_date', 'landline', 'cellphone', 'phone', 'id_birth_city', 'direction', 'id_neighborhood', 'id_gender', 'id_tutor', 'birth_date')->where('id_state', 1)->get();
 
         $excel = array();
         $motivo;
@@ -2370,7 +2370,7 @@ class perfilEstudianteController extends Controller
         }
         $exportar = new SabanaExport([$excel]);
 
-        return Excel::download($exportar, "sábana_export.xlsx");   
+        return Excel::download($exportar, "sábana_secretaía.xlsx");   
     }
 
    

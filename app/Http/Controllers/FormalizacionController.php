8b4f18e2f1318080620e45355fb9ebbeae6e398a
StevenDavila <72047032+StevenDavila@users.noreply.github.com>
2022-04-27 18:34:30 -0500

    Update FormalizacionController.php

--- app/Http/Controllers/FormalizacionController.php
+++ app/Http/Controllers/FormalizacionController.php
@@ -21,32 +21,12 @@ class FormalizacionController extends Controller
 
     public function formalizacionDatos(){
         
-        
-        $datosFormalizacion = DB::select("select student_profile.id, student_profile.name, student_profile.lastname,student_profile.document_number,
-        (SELECT groups.name FROM groups WHERE student_groups.id_group = groups.id) as namegrupo,
-        (SELECT cohorts.name FROM cohorts WHERE cohorts.id = groups.id_cohort) as cohorte,
-        formalizations.id_student,formalizations.acceptance_v1, formalizations.acceptance_v2, formalizations.tablets_v1, formalizations.tablets_v2, formalizations.serial_tablet 
-        FROM student_profile, formalizations, groups, student_groups
-        WHERE student_profile.id = student_groups.id_student
-        AND student_groups.id_group = groups.id
-        AND student_profile.id = formalizations.id_student
-        AND student_profile.id_state = 1");
-        
-        
+        $datosFormalizacion = Formalization::formalizacion();
 
         return datatables()->of($datosFormalizacion)->toJson();
     }
 
     public function index(){
-        /*$datosFormalizacion = DB::select("select student_profile.id, student_profile.name, student_profile.lastname,student_profile.document_number,
-        (SELECT groups.name FROM groups WHERE student_groups.id_group = groups.id) as namegrupo,
-        (SELECT cohorts.name FROM cohorts WHERE cohorts.id = groups.id_cohort) as cohorte,
-        formalizations.id_student,formalizations.acceptance_v1, formalizations.acceptance_v2, formalizations.tablets_v1, formalizations.tablets_v2
-        FROM student_profile, formalizations, groups, student_groups
-        WHERE student_profile.id = student_groups.id_student
-        AND student_groups.id_group = groups.id
-        AND student_profile.id = formalizations.id_student");       
-        return datatables()->of($datosFormalizacion)->toJson();*/
 
         return view('perfilEstudiante.indexFormalizacion');
     }

0dfc2222991d47848d0c1025485bfed9a7d44375
StevenDavila <72047032+StevenDavila@users.noreply.github.com>
2022-05-01 10:04:14 -0500

    Update index.blade.php

--- resources/views/perfilEstudiante/index.blade.php
+++ resources/views/perfilEstudiante/index.blade.php
@@ -43,7 +43,7 @@
         <div class="btn-group">
             <div class="filtroCohortes col-xs-6 col-md-12 col-sm-6">
                 
-                <label>LINEA 1</label>&nbsp;<input type="checkbox" name="check" value="LINEA 1" id="linea_1">&nbsp;&nbsp;&nbsp;&nbsp;
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<label>LINEA 1</label>&nbsp;<input type="checkbox" name="check" value="LINEA 1" id="linea_1">&nbsp;&nbsp;&nbsp;&nbsp;
                 <label>LINEA 2</label>&nbsp;<input type="checkbox" name="check" value="LINEA 2" id="linea_2">&nbsp;&nbsp;&nbsp;&nbsp;
                 <label>LINEA 3</label>&nbsp;<input type="checkbox" name="check" value="LINEA 3" id="linea_3">
                 

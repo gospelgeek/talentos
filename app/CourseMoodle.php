55367e1e0aaf4bd1a19d33291b35e94d4c043500
juliantello <95248642+juliantello@users.noreply.github.com>
2022-05-17 16:46:22 -0500

    Update CourseMoodle.php

--- app/CourseMoodle.php
+++ app/CourseMoodle.php
@@ -27,15 +27,64 @@ class CourseMoodle extends Model
         return $this->hasMany(SessionCourse::class, 'attendance_id','attendance_id');
     }
 
-    public static function asistencias($id_group,$id_moodle,$fecha_inicial,$fecha_final){
+    public static function asistencias_virtuales($id_group,$id_moodle,$fecha_inicial,$fecha_final){
         if($fecha_final != null && $fecha_final != null){
-            $asistencias = DB::select("select course_moodles.fullname,COUNT(*) as Total FROM `course_moodles`,session_courses,attendance_students WHERE course_moodles.group_id = '".$id_group."' and session_courses.attendance_id = course_moodles.attendance_id and attendance_students.session_id = session_courses.session_id
-            and session_courses.sessdate between '".$fecha_inicial."' and '".$fecha_final."'
-            and attendance_students.grade = 'P' and attendance_students.id_moodle = '".$id_moodle."' GROUP BY course_moodles.fullname");
+            $asistencias = DB::select("select course_moodles.fullname,COUNT(*) as Total 
+                            FROM `course_moodles`,session_courses,attendance_students 
+                            WHERE course_moodles.group_id = '".$id_group."' 
+                            and session_courses.attendance_id = course_moodles.attendance_id 
+                            and attendance_students.session_id = session_courses.session_id
+                            and session_courses.sessdate between '".$fecha_inicial."' 
+                            and '".$fecha_final."'
+                            and (attendance_students.grade = 'P' or attendance_students.grade = 'R')
+                            and attendance_students.id_moodle = '".$id_moodle."'
+                            and DAYNAME(session_courses.sessdate) != 'Saturday' 
+                            GROUP BY course_moodles.fullname
+                            ");
         }else{
-            $asistencias = DB::select("select course_moodles.fullname,COUNT(*) as Total FROM `course_moodles`,session_courses,attendance_students WHERE course_moodles.group_id = '".$id_group."' and session_courses.attendance_id = course_moodles.attendance_id and attendance_students.session_id = session_courses.session_id
-                and attendance_students.grade = 'P' and attendance_students.id_moodle = '".$id_moodle."' 
-                GROUP BY course_moodles.fullname");
+            $asistencias = DB::select("select course_moodles.fullname,COUNT(*) as Total 
+                            FROM `course_moodles`,session_courses,attendance_students 
+                            WHERE course_moodles.group_id = '".$id_group."' 
+                            and session_courses.attendance_id = course_moodles.attendance_id 
+                            and attendance_students.session_id = session_courses.session_id
+                            and attendance_students.grade = 'P' 
+                            and attendance_students.id_moodle = '".$id_moodle."'
+                            and DAYNAME(session_courses.sessdate) != 'Saturday' 
+                            GROUP BY course_moodles.fullname");
+
+        }
+
+        if($asistencias != null){
+            return $asistencias;
+        }else{
+            return null;
+        }
+    }
+
+    public static function asistencias_presenciales($id_group,$id_moodle,$fecha_inicial,$fecha_final){
+        if($fecha_final != null && $fecha_final != null){
+            $asistencias = DB::select("select course_moodles.fullname,COUNT(*) as Total 
+                            FROM `course_moodles`,session_courses,attendance_students 
+                            WHERE course_moodles.group_id = '".$id_group."' 
+                            and session_courses.attendance_id = course_moodles.attendance_id 
+                            and attendance_students.session_id = session_courses.session_id
+                            and session_courses.sessdate between '".$fecha_inicial."' 
+                            and '".$fecha_final."'
+                            and (attendance_students.grade = 'P' or attendance_students.grade = 'R')
+                            and attendance_students.id_moodle = '".$id_moodle."'
+                            and DAYNAME(session_courses.sessdate) = 'Saturday' 
+                            GROUP BY course_moodles.fullname
+                            ");
+        }else{
+            $asistencias = DB::select("select course_moodles.fullname,COUNT(*) as Total 
+                            FROM `course_moodles`,session_courses,attendance_students 
+                            WHERE course_moodles.group_id = '".$id_group."' 
+                            and session_courses.attendance_id = course_moodles.attendance_id 
+                            and attendance_students.session_id = session_courses.session_id
+                            and attendance_students.grade = 'P' 
+                            and attendance_students.id_moodle = '".$id_moodle."'
+                            and DAYNAME(session_courses.sessdate) = 'Saturday' 
+                            GROUP BY course_moodles.fullname");
 
         }
 

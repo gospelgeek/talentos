880521a80f710190a1ba1689e71babbb0cd9c152
juliantello <95248642+juliantello@users.noreply.github.com>
2022-05-31 09:24:16 -0500

    Add files via upload

---
+++ resources/views/perfilEstudiante/Asistencias/Individuales/modal/detalles_sesiones.blade.php
@@ -0,0 +1,26 @@
+<div class="modal" data-refresh="true"  tabindex="-1" id="modal_sesiones">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+      <div id="recargar" class="modal-body">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 id="nombre" style="text-align:center;font-size : 30px;font-weight: bolder;"></h4>
+        <div  class="table-responsive">
+        <table id="sesiones_tabla" class="table table-bordered table-striped">
+          <caption id="mensaje" style="caption-side: top;text-align:center;"></caption>
+          <thead>
+            <td>Sesion</td>
+            <td>Asistio</td>
+          </thead>
+          <tbody id="sesiones">
+          </tbody>
+        </table>
+      </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+                        

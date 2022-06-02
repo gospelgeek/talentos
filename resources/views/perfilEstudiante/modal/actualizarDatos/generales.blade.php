7f48b60d2ecf551020deffe4246287e23f88791c
StevenDavila <72047032+StevenDavila@users.noreply.github.com>
2022-06-02 18:21:46 -0500

    Update generales.blade.php

--- resources/views/perfilEstudiante/modal/actualizarDatos/generales.blade.php
+++ resources/views/perfilEstudiante/modal/actualizarDatos/generales.blade.php
@@ -28,8 +28,8 @@
                       {!!Form::text('lastname', $verDatosPerfil ? $verDatosPerfil->lastname : null,['id'=>'apellido','class'=>'form-control','required','placeholder'=>'Apellidos'])!!}  
                       </div>
                       <div class="col-xs-12 col-sm-4 form-group {{$errors->has('body') ? 'has-errors' : ''}}">
-                      {!!Form::label('birth_date','Fecha nacimiento: ')!!}
-                      {!!Form::date('birth_date',$verDatosPerfil ? $verDatosPerfil->birth_date : null,['id'=>'fechaNacimiento', 'class'=>'form-control','placeholder'=>'Fecha nacimiento'])!!}   
+                      {!!Form::label('first_name', 'Nombre de Pila: ')!!}
+                      {!!Form::text('first_name', $verDatosPerfil ? $verDatosPerfil->first_name : null,['id'=>'nombre_pilA','class'=>'form-control','required','placeholder'=>'Nombre de Pila'])!!}  
                       </div>
                   </div>
                   <div class="row">
@@ -42,11 +42,15 @@
                       {!!Form::text('document_number', $verDatosPerfil ? $verDatosPerfil->document_number : null,['id'=>'numeroDocumento','class'=>'form-control','required','placeholder'=>'Numero documento'])!!}  
                       </div>
                       <div class="col-xs-12 col-sm-4 form-group {{$errors->has('body') ? 'has-errors' : ''}}">
-                      {!!Form::label('document_expedition_date','Fecha de expedicion: ')!!}
-                      {!!Form::date('document_expedition_date',$verDatosPerfil ? $verDatosPerfil->document_expedition_date : null,['id'=>'fechaExpedicion', 'class'=>'form-control','placeholder'=>'Fecha expedicion documento'])!!}   
+                      {!!Form::label('birth_date','Fecha nacimiento: ')!!}
+                      {!!Form::date('birth_date',$verDatosPerfil ? $verDatosPerfil->birth_date : null,['id'=>'fechaNacimiento', 'class'=>'form-control','placeholder'=>'Fecha nacimiento'])!!}   
                       </div>
                   </div>
                   <div class="row">
+                    <div class="col-xs-12 col-sm-4 form-group {{$errors->has('body') ? 'has-errors' : ''}}">
+                      {!!Form::label('birth_date','Fecha nacimiento: ')!!}
+                      {!!Form::date('birth_date',$verDatosPerfil ? $verDatosPerfil->birth_date : null,['id'=>'fechaNacimiento', 'class'=>'form-control','placeholder'=>'Fecha nacimiento'])!!}   
+                      </div>
                       <div class="col-xs-12 col-sm-4 form-group {{$errors->has('body') ? 'has-errors' : ''}}">
                       {!!Form::label('birthcity','Ciudad nacimiento: ')!!}
                       {!!Form::select('birthcity',$ciudad_nacimiento, $verDatosPerfil->birthcity ? $verDatosPerfil->birthcity->id : null,['id'=>'ciudadNacimiento','class'=>'form-control','required','placeholder'=>'Ciudad nacimiento'])!!}   
@@ -54,13 +58,13 @@
                       <div class="col-xs-12 col-sm-4 form-group {{$errors->has('body') ? 'has-errors' : ''}}">
                       {!!Form::label('email','Email: ')!!}
                       {!!Form::email('email', $verDatosPerfil ? $verDatosPerfil->email : null,['id'=>'correo','class'=>'form-control','required','placeholder'=>'Correo electronico'])!!}  
-                      </div> 
-                      <div class="col-xs-12 col-sm-4 form-group {{$errors->has('body') ? 'has-errors' : ''}}">
+                      </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-xs-12 col-sm-4 form-group {{$errors->has('body') ? 'has-errors' : ''}}">
                       {!!Form::label('sex','Sexo: ')!!}
                       {!!Form::select('sexo',$sexo, $verDatosPerfil ? $verDatosPerfil->sex : null,['id'=>'sexo1','class'=>'form-control','required','placeholder'=>'Seleccionar sexo'])!!}  
                       </div> 
-                  </div>
-                  <div class="row">
                       <div class="col-xs-12 col-sm-4 form-group {{$errors->has('body') ? 'has-errors' : ''}}">
                       {!!Form::label('gender','Genero: ')!!}
                       {!!Form::select('genero',$genero, $verDatosPerfil->gender ? $verDatosPerfil->gender->id : null,['id'=>'generoE','class'=>'form-control','required','placeholder'=>'Genero'])!!}  
@@ -69,26 +73,34 @@
                       {!!Form::label('cellphone','Celular: ')!!}
                       {!!Form::text('cellphone', $verDatosPerfil ? $verDatosPerfil->cellphone : null,['id'=>'celular','class'=>'form-control','required','placeholder'=>'Celular'])!!}  
                       </div>
-                      <div class="col-xs-12 col-sm-4 form-group {{$errors->has('body') ? 'has-errors' : ''}}">
-                      {!!Form::label('neighborhood','Barrio de residencia: ')!!}
-                      {!!Form::select('barrio',$barrio, $verDatosPerfil->neighborhood ? $verDatosPerfil->neighborhood->id : null,['id'=>'barrioV','class'=>'form-control','required','placeholder'=>'Genero'])!!}  
-                      </div>
                   </div>
                   <div class="row">
+                    <div class="col-xs-12 col-sm-4 form-group {{$errors->has('body') ? 'has-errors' : ''}}">
+                      {!!Form::label('phone','Numero alternativo: ')!!}
+                      {!!Form::text('phone', $verDatosPerfil ? $verDatosPerfil->phone : null,['id'=>'telefono','class'=>'form-control','required','placeholder'=>'Telefono'])!!}
+                      </div>
                       <div class="col-xs-12 col-sm-4 form-group {{$errors->has('body') ? 'has-errors' : ''}}">
                       {!!Form::label('direction','Direccion: ')!!}
                       {!!Form::text('direction', $verDatosPerfil ? $verDatosPerfil->direction : null,['id'=>'direccion12','class'=>'form-control','required','placeholder'=>'Direccion'])!!}  
                       </div>
                       <div class="col-xs-12 col-sm-4 form-group {{$errors->has('body') ? 'has-errors' : ''}}">
-                      {!!Form::label('phone','Numero alternativo: ')!!}
-                      {!!Form::text('phone', $verDatosPerfil ? $verDatosPerfil->phone : null,['id'=>'telefono','class'=>'form-control','required','placeholder'=>'Telefono'])!!}
+                      {!!Form::label('neighborhood','Barrio recidencia: ')!!}
+                      {!!Form::select('barrio',$barrio, $verDatosPerfil->neighborhood ? $verDatosPerfil->neighborhood->id : null,['id'=>'barrioV','class'=>'form-control','required','placeholder'=>'Genero'])!!}  
                       </div>
-                      <div class="col-xs-12 col-sm-4 form-group {{$errors->has('body') ? 'has-errors' : ''}}">
+                  </div>
+                  <div class="row">
+                    @if(auth()->user()->rol_id == 1)
+                    <div class="col-xs-12 col-sm-4 form-group {{$errors->has('body') ? 'has-errors' : ''}}">
                       {!!Form::label('student_code','Codigo estudiante: ')!!}
                       {!!Form::text('student_code', $verDatosPerfil ? $verDatosPerfil->student_code : null,['id'=>'codEstu','class'=>'form-control','required','placeholder'=>'Codigo estudiante'])!!}  
+                    </div>
+                    @endif
+                    <div class="col-xs-12 col-sm-4 form-group {{$errors->has('body') ? 'has-errors' : ''}}">
+                      {!!Form::label('emergency_contact','Contacto de emergencia: ')!!}
+                      {!!Form::text('emergency_contact', $verDatosPerfil ? $verDatosPerfil->emergency_contact : null,['id'=>'emergencia','class'=>'form-control','required','placeholder'=>'Contacto emergencia'])!!}
                       </div>
                   </div>
-                  </div>
+                </div>
               </div>
 
               <div class="modal-footer">

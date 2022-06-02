97e75c1403f1e729fdda9190008a0bf8c73dc7ed
StevenDavila <72047032+StevenDavila@users.noreply.github.com>
2022-06-02 18:16:45 -0500

    Update createStudent.blade.php

--- resources/views/perfilEstudiante/modal/createStudent.blade.php
+++ resources/views/perfilEstudiante/modal/createStudent.blade.php
@@ -25,14 +25,17 @@
                           {!!Form::label('lastname','Apellidos: *')!!}
                           {!!Form::text('lastname',null,['id'=>'apellidos1','class'=>'form-control','required','placeholder'=>'Apellidos'])!!}  
                         </div>  
-                        
+                        <div class="col-xs-12 col-sm-4 form-group {{$errors->has('body') ? 'has-errors' : ''}}">
+                          {!!Form::label('first_name','Nombre de Pila: ')!!}
+                          {!!Form::text('first_name',null,['id'=>'nombre_pila','class'=>'form-control','required','placeholder'=>'Nombre de Pila'])!!}  
+                        </div>
+                      </div>
+
+                      <div class="row">
                         <div class="col-xs-12 col-sm-4 form-group {{$errors->has('body') ? 'has-errors' : ''}}">
                           {!!Form::label('id_document_type','Selecionar tipo documento: *')!!}
                           {!!Form::select('id_document_type',$tipo_documento, null,['id'=>'documenttype','class'=>'form-control','required','placeholder'=>'Seleccionar tipo documento'])!!}  
                         </div> 
-                      </div>
-
-                      <div class="row">
                         <div class="col-xs-12 col-sm-4 form-group {{$errors->has('body') ? 'has-errors' : ''}}">
                           {!!Form::label('document_number','Número documento: *')!!}
                           {!!Form::number('document_number',null,['id'=>'document_number','class'=>'form-control','required','placeholder'=>'Numero documento'])!!}  
@@ -41,13 +44,13 @@
                           {!!Form::label('birth_date','Fecha nacimiento: ')!!}
                           {!!Form::date('birth_date',null,['id'=>'fecha_nac','class'=>'form-control','required','placeholder'=>'Fecha nacimiento'])!!}  
                         </div>
+                      </div>
+
+                      <div class="row">
                         <div class="col-xs-12 col-sm-4 form-group {{$errors->has('body') ? 'has-errors' : ''}}">
                           {!!Form::label('id_birth_department','Departamento nacimiento: ')!!}
                           {!!Form::select('id_birth_department',$depNacimiento, null,['id'=>'depNacimiento','class'=>'form-control','required','placeholder'=>'Departameno nacimiento'])!!}  
                         </div>
-                      </div>
-
-                      <div class="row">
                         <div class="col-xs-12 col-sm-4 form-group {{$errors->has('body') ? 'has-errors' : ''}}">
                           {!!Form::label('id_birth_city','Ciudad nacimiento: ')!!}
                           {!!Form::select('id_birth_city',$muni_nacimiento, null,['id'=>'muni_nacimiento','class'=>'form-control','required','placeholder'=>'Seleccionar ciudad' ,'style'=>' '])!!}
@@ -56,17 +59,24 @@
                           {!!Form::label('email','Email: *')!!}
                           {!!Form::email('email',null,['id'=>'correo','class'=>'form-control','required','placeholder'=>'Correo elctronico'])!!}  
                         </div>
-                        <div class="col-xs-12 col-sm-4 form-group {{$errors->has('body') ? 'has-errors' : ''}}">
-                          {!!Form::label('sex','Sexo: *')!!}
-                          {!!Form::select('sex', $sexo, null,['id'=>'sex','class'=>'form-control','required','placeholder'=>'Seleccionar sexo' ,'style'=>' '])!!}
-                        </div>
                       </div>
 
                       <div class="row">
                         <div class="col-xs-12 col-sm-4 form-group {{$errors->has('body') ? 'has-errors' : ''}}">
+                          {!!Form::label('sex','Sexo: *')!!}
+                          {!!Form::select('sex', $sexo, null,['id'=>'sex','class'=>'form-control','required','placeholder'=>'Seleccionar sexo' ,'style'=>' '])!!}
+                        </div>
+                        <div class="col-xs-12 col-sm-4 form-group {{$errors->has('body') ? 'has-errors' : ''}}">
                           {!!Form::label('id_gender','Género: ')!!}
                           {!!Form::select('id_gender', $genero,null,['id'=>'gener','placeholder'=>'Género','class'=>'form-control','required'])!!}
                         </div>
+                        <div class="col-xs-12 col-sm-4 form-group {{$errors->has('body') ? 'has-errors' : ''}}">
+                          {!!Form::label('student_code','Código estudiante: ')!!}
+                          {!!Form::text('student_code',null,['id'=>'codStu','class'=>'form-control','required','placeholder'=>'Codigo estudiante'])!!}
+                        </div>
+                      </div>
+
+                      <div class="row">
                         <div class="col-xs-12 col-sm-4 form-group {{$errors->has('body') ? 'has-errors' : ''}}">
                           {!!Form::label('id_commune','Comuna: ')!!}
                           {!!Form::select('id_commune',$comunas, null,['id'=>'comunaResidencia','class'=>'form-control','required','placeholder'=>'Seleccionar Comuna' ,'style'=>' '])!!}
@@ -75,20 +85,9 @@
                           {!!Form::label('id_neighborhood','Barrio: ')!!}
                           {!!Form::select('id_neighborhood',$barrios, null,['id'=>'barrioResidencia','class'=>'form-control','required','placeholder'=>'Seleccionar Barrio' ,'style'=>' '])!!}
                         </div>
-                      </div>
-
-                      <div class="row">
                         <div class="col-xs-12 col-sm-4 form-group {{$errors->has('body') ? 'has-errors' : ''}}">
                           {!!Form::label('direction','Dirección: ')!!}
                           {!!Form::text('direction',null,['id'=>'direCcion','class'=>'form-control','required','placeholder'=>'Direccion de residencia'])!!}
-                        </div>
-                        <div class="col-xs-12 col-sm-4 form-group {{$errors->has('body') ? 'has-errors' : ''}}">
-                          {!!Form::label('id_tutor','Tutor: ')!!}
-                          {!!Form::select('id_tutor',$tutor,null,['id'=>'tutoR','class'=>'form-control','required','placeholder'=>'Tutor'])!!}
-                        </div>
-                        <div class="col-xs-12 col-sm-4 form-group {{$errors->has('body') ? 'has-errors' : ''}}">
-                          {!!Form::label('student_code','Código estudiante: ')!!}
-                          {!!Form::text('student_code',null,['id'=>'codStu','class'=>'form-control','required','placeholder'=>'Codigo estudiante'])!!}
                         </div>
                       </div>
 
@@ -102,8 +101,8 @@
                           {!!Form::select('id_group', $grupo, null,['id'=>'groupC','class'=>'form-control','required','placeholder'=>'Seleccionar Grupo' ,'style'=>' '])!!}
                         </div>
                         <div class="col-xs-12 col-sm-4 form-group {{$errors->has('body') ? 'has-errors' : ''}}">
-                          {!!Form::label('id_moodle','ID moodle: ')!!}
-                          {!!Form::number('id_moodle',null,['id'=>'mooDle','class'=>'form-control','required','placeholder'=>'Moodle'])!!}
+                          {!!Form::label('id_tutor','Tutor: ')!!}
+                          {!!Form::select('id_tutor',$tutor,null,['id'=>'tutoR','class'=>'form-control','required','placeholder'=>'Tutor'])!!}
                         </div>
                       </div>
 
@@ -116,7 +115,10 @@
                           {!!Form::label('phone','Numero telefónico alternativo: ')!!}
                           {!!Form::number('phone',null,['id'=>'telefono','class'=>'form-control','required','placeholder'=>'Numero alternativo'])!!}
                         </div>
-                        
+                        <div class="col-xs-12 col-sm-4 form-group {{$errors->has('body') ? 'has-errors' : ''}}">
+                          {!!Form::label('id_moodle','ID moodle: ')!!}
+                          {!!Form::number('id_moodle',null,['id'=>'mooDle','class'=>'form-control','required','placeholder'=>'Moodle'])!!}
+                        </div>
                       </div>
                     </div>
                   </div>

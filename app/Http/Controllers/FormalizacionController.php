aff68dc2a8fd0312092841c33a38d6470d385e32
StevenDavila <72047032+StevenDavila@users.noreply.github.com>
2022-06-06 11:27:12 -0500

    Update FormalizacionController.php

--- app/Http/Controllers/FormalizacionController.php
+++ app/Http/Controllers/FormalizacionController.php
@@ -42,75 +42,76 @@ class FormalizacionController extends Controller
     $data = Formalization::findOrFail($id);
         
         if ($request->ajax()) {
-            
+
             if($request->checkAceptacion == 'true') {
                     
-                            if($request->acceptance_v2 != null){
-                                $data->acceptance_v2 = $request['acceptance_v2'];
-                            }else{
-                                $data->acceptance_v2 = 'SI';
-                            }      
-                   }
+                if($request->acceptance_v2 != null){
+                    $data->acceptance_v2 = $request['acceptance_v2'];
+                }else{
+                    $data->acceptance_v2 = 'SI';
+                }      
+            }
 
-                    if($request->checkTablet == 'true') {
+            if($request->checkTablet == 'true') {
                     
-                        if($request->tablets_v2 != null){
-                            $data->tablets_v2 = $request['tablets_v2'];
-                        }else{
-                            $data->tablets_v2 = 'SI';
-                        }      
-                    }
-
-                    $data->serial_tablet = $request['serial_tablet'];
-                    $data->kit_date = $request['date_kit'];
-                    $data->observations = $request['observaciones'];
+                if($request->tablets_v2 != null){
+                    $data->tablets_v2 = $request['tablets_v2'];
+                }else{
+                    $data->tablets_v2 = 'SI';
+                }      
+            }
+            $data->acceptance_v2 = $request['acceptance_v2'];
+            $data->tablets_v2 = $request['tablets_v2'];
+            $data->serial_tablet = $request['serial_tablet'];
+            $data->kit_date = $request['date_kit'];
+            $data->observations = $request['observaciones'];
                 
-                    $pre_registro;
-                    if($request['pre_registro_icfes'] !== null){
-                        $pre_registro = true;
-                        $data->pre_registration_icfes = $pre_registro; 
-                    }else if($request['pre_registro_icfes'] == null) {
-                        $pre_registro = false;
-                        $data->pre_registration_icfes = $pre_registro;
-                    }
+            $pre_registro;
+            if($request['pre_registro_icfes'] !== null){
+                $pre_registro = true;
+                $data->pre_registration_icfes = $pre_registro; 
+            }else if($request['pre_registro_icfes'] == null) {
+                $pre_registro = false;
+                $data->pre_registration_icfes = $pre_registro;
+            }
 
-                    $inscripcion_icfes;
-                    if($request['inscripcion_icfes'] !== null){
-                        $inscripcion_icfes = true;
-                        $data->inscription_icfes = $inscripcion_icfes;
-                    }else if($request['inscripcion_icfes'] == null){
-                        $inscripcion_icfes = false;
-                        $data->inscription_icfes = $inscripcion_icfes;
-                    }
+            $inscripcion_icfes;
+            if($request['inscripcion_icfes'] !== null){
+                $inscripcion_icfes = true;
+                $data->inscription_icfes = $inscripcion_icfes;
+            }else if($request['inscripcion_icfes'] == null){
+                $inscripcion_icfes = false;
+                $data->inscription_icfes = $inscripcion_icfes;
+            }
 
-                    $presento_icfes;
-                    if($request['presento_icfes'] !== null){
-                        $presento_icfes = true;
-                        $data->presented_icfes = $presento_icfes;
-                    }else if($request['presento_icfes'] == null){
-                        $presento_icfes = false;
-                        $data->presented_icfes = $presento_icfes;
-                    }
+            $presento_icfes;
+            if($request['presento_icfes'] !== null){
+                $presento_icfes = true;
+                $data->presented_icfes = $presento_icfes;
+            }else if($request['presento_icfes'] == null){
+                $presento_icfes = false;
+                $data->presented_icfes = $presento_icfes;
+            }
 
-                $ver = EconomicalSupport::where('id_student', $request['id'])->exists();
+            $ver = EconomicalSupport::where('id_student', $request['id'])->exists();
 
-                if($ver == true){
-                    $economico_id = EconomicalSupport::select('id')->where('id_student', $request['id'])->first();
-                    $data_apoyo = EconomicalSupport::findOrfail($economico_id->id);
-                    $data_apoyo->date = $request['fecha_apoyo'];
-                    $data_apoyo->url_banco = $request['banco_url'];
-                    $data_apoyo->monto = $request['monto'];
+            if($ver == true){
+                $economico_id = EconomicalSupport::select('id')->where('id_student', $request['id'])->first();
+                $data_apoyo = EconomicalSupport::findOrfail($economico_id->id);
+                $data_apoyo->date = $request['fecha_apoyo'];
+                $data_apoyo->url_banco = $request['banco_url'];
+                $data_apoyo->monto = $request['monto'];
                     
-                    $data_apoyo->save();
-                }else if($ver == false){
+                $data_apoyo->save();
+            }else if($ver == false){
                     
-                    $apoyo_economico = EconomicalSupport::create([
-                        'id_student'    => $request['id'],
-                        'date'          => $request['fecha_apoyo'],
-                        'url_banco'     => $request['banco_url'],
-                        'monto'         => $request['monto'],
-                    ]);
-                }
+                $apoyo_economico = EconomicalSupport::create([
+                    'id_student'    => $request['id'],
+                    'date'          => $request['fecha_apoyo'],
+                    'url_banco'     => $request['banco_url'],
+                    'monto'         => $request['monto'],
+                ]);
+            }
         };
 
         $data->save();

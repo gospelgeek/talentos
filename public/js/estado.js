a5a9e21f9567e9fe9e4e37784b5b0d40a6b1ee5c
juliantello <95248642+juliantello@users.noreply.github.com>
2022-06-06 04:34:17 -0500

    Update estado.js

--- public/js/estado.js
+++ public/js/estado.js
@@ -23,6 +23,7 @@ function abrirmodal(e) {
             $('#CMotivo').hide();
             $('#CUrl').hide();
             $('#CBoton').hide();
+            $('#Cfecha').hide();
             var valor = $('#estadoN').val();
             //console.log(valor)
             $('#idE').val(result[0].id);
@@ -37,16 +38,18 @@ function abrirmodal(e) {
                   $('#CMotivo').hide();
                   $('#CUrl').hide();
                   $('#CBoton').hide();
+                  $('#Cfecha').hide();
                 }
                 if(valor == 3 || valor == 2 || valor == 5){
                   $('#CMotivo').show();
                   $('#Cobservacion').show();
                   $('#CUrl').show();
                   $('#CBoton').show();
+                  $('#Cfecha').show();
                   $('#idE').val(result[0].id); 
                   $("#CMotivo").val(result[0].withdrawals.id_reasons);
                   $("#CUrl").val(result[0].withdrawals.url);
-
+                  $("#Cfecha").val(result[0].withdrawals.fecha);
                   if(result[0].withdrawals.url != null){
                     document.getElementById('CBoton').setAttribute('href', result[0].withdrawals.url);
                   }else{
@@ -58,16 +61,20 @@ function abrirmodal(e) {
                 if(valor == 4){
                   $('#idE').val(result[0].id);
                   $("#Cobservacion").val(result[0].withdrawals.observation);
+                  $("#Cfecha").val(result[0].withdrawals.fecha);
                   $('#Cobservacion').show();
                   $('#CMotivo').hide();
                   $('#CUrl').hide();
-                  $('#CBoton').hide();               
+                  $('#CBoton').hide();
+                  $('#Cfecha').show();               
                 }
                 if(valor == 1){
                   $('#Cobservacion').hide();
                   $('#CMotivo').hide();
                   $('#CUrl').hide();
                   $('#CBoton').hide();
+                  $('#Ffecha').hide();
+                  $('#Cfecha').hide();
                 }
 
                  
@@ -101,6 +108,7 @@ function abrirmodal_ver(e) {
             $('#VMotivo').hide();
             $('#VUrl').hide();
             $('#VBoton').hide();
+            $('#Vfecha').hide();
             var valor = $('#estadoV').val();
             //console.log(valor)
             $('#idV').val(result[0].id);
@@ -115,15 +123,18 @@ function abrirmodal_ver(e) {
                   $('#VMotivo').hide();
                   $('#VUrl').hide();
                   $('#VBoton').hide();
+                  $('#Vfecha').hide();
                 }
                 if(valor == 3 || valor == 2 || valor == 5){
                   $('#VMotivo').show();
                   $('#Vobservacion').show();
                   $('#VUrl').show();
                   $('#VBoton').show();
+                  $('#Vfecha').show();
                   $('#idV').val(result[0].id); 
                   $("#VMotivo").val(result[0].withdrawals.id_reasons);
                   $("#VUrl").val(result[0].withdrawals.url);
+                  $("#Vfecha").val(result[0].withdrawals.fecha);
 
                   if(result[0].withdrawals.url != null){
                     document.getElementById('VBoton').setAttribute('href', result[0].withdrawals.url);
@@ -136,16 +147,19 @@ function abrirmodal_ver(e) {
                 if(valor == 4){
                   $('#idV').val(result[0].id);
                   $("#Vobservacion").val(result[0].withdrawals.observation);
+                  $("#Vfecha").val(result[0].withdrawals.fecha);
                   $('#Vobservacion').show();
                   $('#VMotivo').hide();
                   $('#VUrl').hide();
-                  $('#VBoton').hide();               
+                  $('#VBoton').hide();
+                  $('#Vfecha').show();               
                 }
                 if(valor == 1){
                   $('#Vobservacion').hide();
                   $('#VMotivo').hide();
                   $('#VUrl').hide();
                   $('#VBoton').hide();
+                  $('#Vfecha').hide();
                 }
 
                  
@@ -164,24 +178,28 @@ $("#estadoN").on('change',function(event) {
             $('#CMotivo').hide();
             $('#CUrl').hide();
             $('#CBoton').hide();
+            $('#Cfecha').hide();
           }
           if(valor == 3 || valor == 2 || valor == 5){
             $('#CMotivo').show();
             $('#Cobservacion').show();
             $('#CUrl').show();
             $('#CBoton').show();
+            $('#Cfecha').show();
           }
           if(valor == 4){
             $('#Cobservacion').show();
             $('#CMotivo').hide();
             $('#CUrl').hide();
-            $('#CBoton').hide();               
+            $('#CBoton').hide();
+            $('#Cfecha').show();               
           }
           if(valor == 1){
             $('#Cobservacion').hide();
             $('#CMotivo').hide();
             $('#CUrl').hide();
             $('#CBoton').hide();
+            $('#Cfecha').hide();
           }
 });
 
@@ -193,13 +211,14 @@ $('.boton_update_estado').click(function(e) {
   $.ajax({
   //ruta manual
     url:'/update_estado/'+ idEstado,
-    type:'PUT',
+    type:'GET',
     data:{
       '_token': $('input[name=_token]').val(),
       'id_state': $("#estadoN").val(),
       'id_reasons': $("#CMotivo").val(),
       'observation': $("#Cobservacion").val(),
       'url':         $("#CUrl").val(),
+      'fecha':       $("#Cfecha").val(),
     },
     success:function(msj) {
       $('#modal_crear_estado').modal('hide');
@@ -215,7 +234,7 @@ $('.boton_update_estado').click(function(e) {
         //$("#msj").append("<ul><li>"+field.errors.calendario_nombre+"</li><li>"+field.errors.calendario_semestre+"</li></ul>");   
         console.log(mensajeError)
       });
-      $("#msj-error-agendamiento").html("<ul>"+mensajeError+"</ul>").fadeIn();         
+      $("#msj-error-en-estado").html("<ul>"+mensajeError+"</ul>").fadeIn();         
     },       
   });
   });
@@ -265,6 +284,3 @@ $('.boton_update_estado').click(function(e) {
                       },
                     ]
       });*/
-
-      
-

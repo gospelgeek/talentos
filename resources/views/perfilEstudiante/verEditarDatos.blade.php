5db85f5ebdb208f7e6c72de3549e07886e0af714
StevenDavila <72047032+StevenDavila@users.noreply.github.com>
2022-06-07 13:53:54 -0500

    Update verEditarDatos.blade.php

--- resources/views/perfilEstudiante/verEditarDatos.blade.php
+++ resources/views/perfilEstudiante/verEditarDatos.blade.php
@@ -882,9 +882,13 @@
             				</div>
             			</div>
             			<div class="btn-group">
-            				<div class="col-xs-6 col-md-12 col-sm-6">
-            					<a class="btn btn-primary elevation-5 btn-sm mt-3 mb-3 fa fa-plus float-left" title="Nuevo registro" id="nuevo_registro" onclick="apoyo_economico();"></a>
-           					</div>
+            				@if(auth()->user()->rol_id == 4 || auth()->user()->rol_id == 1)
+            					<div class="btn-group">
+            						<div class="col-xs-6 col-md-12 col-sm-6">
+            							<a class="btn btn-primary elevation-5 btn-sm mt-3 mb-3 fa fa-plus float-left" title="Nuevo registro" id="nuevo_registro" onclick="apoyo_economico();"></a>
+           							</div>
+           						</div>
+           					@endif
            				</div>
            			</div>
            			<div class="row" id="inputs">
@@ -1019,9 +1023,13 @@
             				</div>
             			</div>
             			<div class="btn-group">
-            				<div class="col-xs-6 col-md-12 col-sm-6">
-            					<a class="btn btn-primary elevation-5 btn-sm mt-3 mb-3 fa fa-plus float-left" title="Nuevo registro" id="nuevo_registro" onclick="apoyo_economico();"></a>
-           					</div>
+            				@if(auth()->user()->rol_id == 4 || auth()->user()->rol_id == 1)
+            					<div class="btn-group">
+            						<div class="col-xs-6 col-md-12 col-sm-6">
+            							<a class="btn btn-primary elevation-5 btn-sm mt-3 mb-3 fa fa-plus float-left" title="Nuevo registro" id="nuevo_registro" onclick="apoyo_economico();"></a>
+           							</div>
+           						</div>
+           					@endif
            				</div>
            			</div>
            			<div class="row" id="inputs">

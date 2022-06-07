ae9c065907bca4411fd02a88a8ff9fb3e427a7cf
StevenDavila <72047032+StevenDavila@users.noreply.github.com>
2022-06-07 13:00:09 -0500

    Update FormalizacionController.php

--- app/Http/Controllers/FormalizacionController.php
+++ app/Http/Controllers/FormalizacionController.php
@@ -5,14 +5,19 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\View;
+use App\Http\Controllers\Auth;
 use App\Formalization;
 use App\perfilEstudiante;
 use App\EconomicalSupport;
 use App\Group;
 use App\Cohort;
 use App\StudentGroup;
+use App\UpdateInformation;
+use App\LogsCrudActions;
+use App\User;
 use Session;
 use Redirect;
+use Response;
 use DB;
 
 class FormalizacionController extends Controller
@@ -36,10 +41,10 @@ class FormalizacionController extends Controller
         return view('perfilEstudiante.indexFormalizacion');
     }
     
-    public function formalizacionupdate($id, Request $request){
-
-       
+   public function formalizacionupdate($id, Request $request){
+  
     $data = Formalization::findOrFail($id);
+    $dataOld = Formalization::findOrFail($id);
         
         if ($request->ajax()) {
 
@@ -93,29 +98,172 @@ class FormalizacionController extends Controller
                 $data->presented_icfes = $presento_icfes;
             }
 
-            $ver = EconomicalSupport::where('id_student', $request['id'])->exists();
+           $apoyo_economico = EconomicalSupport::create([
+                'id_student'    => $request['id'],
+                'date'          => $request['fecha_apoyo'],
+                'url_banco'     => $request['banco_url'],
+                'monto'         => $request['monto'],
+            ]);
 
-            if($ver == true){
-                $economico_id = EconomicalSupport::select('id')->where('id_student', $request['id'])->first();
-                $data_apoyo = EconomicalSupport::findOrfail($economico_id->id);
-                $data_apoyo->date = $request['fecha_apoyo'];
-                $data_apoyo->url_banco = $request['banco_url'];
-                $data_apoyo->monto = $request['monto'];
-                    
-                $data_apoyo->save();
-            }else if($ver == false){
-                    
-                $apoyo_economico = EconomicalSupport::create([
-                    'id_student'    => $request['id'],
-                    'date'          => $request['fecha_apoyo'],
-                    'url_banco'     => $request['banco_url'],
-                    'monto'         => $request['monto'],
-                ]);
-            }
+            $ip = User::getRealIP();
+            $id = auth()->user();
+            
+            $datos = LogsCrudActions::create([
+                'id_user'                  => $id['id'],
+                'rol'                      => $id['rol_id'],
+                'ip'                       => $ip,
+                'id_usuario_accion'        => $apoyo_economico['id'],
+                'actividad_realizada'      => 'NUEVO REGISTRO DE APOYO ECONOMICO',
+            ]);
+            
         };
 
         $data->save();
+
+        $old = array();
+        $new = array();
+
+        if($dataOld->acceptance_v2 != $data->acceptance_v2){
+            $old[] = array('aceptacion' => $dataOld->acceptance_v2);
+            $new[] = array('aceptacion' => $data->acceptance_v2);
+        }
+        if($dataOld->tablets_v2 != $data->tablets_v2){
+            $old[] = array('tablets' => $dataOld->tablets_v2);
+            $new[] = array('tablets' => $data->tablets_v2);
+        }
+        if($dataOld->serial_tablet != $data->serial_tablet){
+            $old[] = array('serial tablet' => $dataOld->serial_tablet);
+            $new[] = array('serial tablet' => $data->serial_tablet);
+        }
+        if($dataOld->kit_date != $data->kit_date){
+            $old[] = array('fecha kit' => $dataOld->kit_date);
+            $new[] = array('fecha kit' => $data->kit_date);
+        }
+        if($dataOld->pre_registration_icfes != $data->pre_registration_icfes){
+            $old[] = array('pre_registro' => $dataOld->pre_registration_icfes);
+            $new[] = array('pre_registro' => $data->pre_registration_icfes);
+        }
+        if($dataOld->inscription_icfes != $data->inscription_icfes){
+            $old[] = array('inscription' => $dataOld->inscription_icfes);
+            $new[] = array('inscription' => $data->inscription_icfes);
+        }
+        if($dataOld->presented_icfes != $data->presented_icfes){
+            $old[] = array('presentó icfes' => $dataOld->presented_icfes);
+            $new[] = array('presentó icfes' => $data->presented_icfes);
+        }
+        if($dataOld->observations != $data->observations){
+            $old[] = array('observaciones' => $dataOld->observations);
+            $new[] = array('observaciones' => $data->observations);
+        }
+
+        if($old != null && $new != null){
+
+            $datos = LogsCrudActions::create([
+                'id_user'                  => $id['id'],
+                'rol'                      => $id['rol_id'],
+                'ip'                       => $ip,
+                'id_usuario_accion'        => $data['id'],
+                'actividad_realizada'      => 'FORMALIZACION ACTUALIZADA',
+            ]);
             
+            $guardarOld = json_encode($old);
+            $guardarNew = json_encode($new);
+
+            $update = UpdateInformation::create([
+                'id_log'              => $datos['id'],
+                'changed_information' => $guardarOld,
+                'new_information'     => $guardarNew,
+            ]);
+        }        
+
         return 2;     
+    }
+    
+    public function apoyo_economico_editar($id, Request $request){
+
+        $apoyos = EconomicalSupport::findOrFail($id);
+
+        if ($request->ajax()) {
+            return Response::json($apoyos);
+        };
+    }
+
+    public function apoyo_economico_update($id, Request $request){
+
+        $data = EconomicalSupport::findOrFail($id);
+        $dataOld = EconomicalSupport::findOrFail($id);
+
+        if($request->ajax()){
+            $data->date      = $request['date'];
+            $data->url_banco = $request['url_banco'];
+            $data->monto     = $request['monto'];
+
+            $data->save();
+
+            $old = array();
+            $new = array();
+
+            if($dataOld->date != $data->date){
+                $old[] = array('fecha apoyo' => $dataOld->date);
+                $new[] = array('fecha apoyo' => $data->date);
+            }
+            if($dataOld->url_banco != $data->url_banco){
+                $old[] = array('banco' => $dataOld->url_banco);
+                $new[] = array('banco' => $data->url_banco);
+            }
+            if($dataOld->monto != $data->monto){
+                $old[] = array('monto' => $dataOld->monto);
+                $new[] = array('monto' => $data->monto);
+            }
+
+            if($old != null && $new != null){
+
+                $ip = User::getRealIP();
+                $id = auth()->user();
+            
+                $datos = LogsCrudActions::create([
+                    'id_user'                  => $id['id'],
+                    'rol'                      => $id['rol_id'],
+                    'ip'                       => $ip,
+                    'id_usuario_accion'        => $data['id'],
+                    'actividad_realizada'      => 'SE ACTUALIZO REGISTRO DE APOYO ECONOMICO',
+                ]);
+
+                $guardarOld = json_encode($old);
+                $guardarNew = json_encode($new);
+
+                $update = UpdateInformation::create([
+                    'id_log'              => $datos['id'],
+                    'changed_information' => $guardarOld,
+                    'new_information'     => $guardarNew,
+                ]);
+            }
+
+        }
+
+        return 1;
+    }
+
+    public function apoyo_economico_delete($id, Request $request){
+        
+        $data = EconomicalSupport::findOrFail($id);
+        $mensaje = 'Apoyo economico eliminado correctamente';
+        if ($request->ajax()) {
+            
+            $ip = User::getRealIP();
+            $id = auth()->user();
+
+            $datos = LogsCrudActions::create([
+                'id_user'                  => $id['id'],
+                'rol'                      => $id['rol_id'],
+                'ip'                       => $ip,
+                'id_usuario_accion'        => $data['id'],
+                'actividad_realizada'      => 'APOYO ECONOMICO ELIMINADO',
+            ]);
+
+            $data->delete();    
+        }
+        
+        return $mensaje;   
     }
 }

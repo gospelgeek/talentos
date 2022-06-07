ad8f1801c48b9e9962f030e0062977c8509846a7
StevenDavila <72047032+StevenDavila@users.noreply.github.com>
2022-06-07 18:41:20 -0500

    Update Session.php

--- app/Session.php
+++ app/Session.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use SoftDeletes;
+use DB;
 
 class Session extends Model
 {
@@ -20,6 +21,91 @@ class Session extends Model
     ];
 
     protected $dates = ['delete_at'];
+
+
+    //consultas sesiones
+    public static function sesiones_linea1(){
+        $data = DB::select("select sessions.id_group, sessions.id_course, sessions.date_session, cohorts.name as linea, groups.name as grupo, courses.name as asignatura
+            FROM sessions
+            INNER JOIN groups ON groups.id = sessions.id_group
+            INNER JOIN cohorts on cohorts.id = groups.id_cohort
+            INNER JOIN courses ON courses.id = sessions.id_course
+            WHERE sessions.id_group BETWEEN 1 AND 40");
+
+        if($data != null) {
+            return $data;
+        }else{
+            return null;
+        }
+
+    }
+
+    public static function sesiones_linea2(){
+        $data = DB::select("select sessions.id_group, sessions.id_course, sessions.date_session, cohorts.name as linea, groups.name as grupo, courses.name as asignatura
+            FROM sessions
+            INNER JOIN groups ON groups.id = sessions.id_group
+            INNER JOIN cohorts on cohorts.id = groups.id_cohort
+            INNER JOIN courses ON courses.id = sessions.id_course
+            WHERE sessions.id_group BETWEEN 1004 AND 1044");
+
+        if($data != null) {
+            return $data;
+        }else{
+            return null;
+        }
+
+    }
+
+    public static function sesiones_linea3(){
+        $data = DB::select("select sessions.id_group, sessions.id_course, sessions.date_session, cohorts.name as linea, groups.name as grupo, courses.name as asignatura
+            FROM sessions
+            INNER JOIN groups ON groups.id = sessions.id_group
+            INNER JOIN cohorts on cohorts.id = groups.id_cohort
+            INNER JOIN courses ON courses.id = sessions.id_course
+            WHERE sessions.id_group BETWEEN 50 AND 89");
+
+        if($data != null) {
+            return $data;
+        }else{
+            return null;
+        }
+
+    }
+
+    public static function grupos($grupo){
+        $data = DB::select("select sessions.id_group, sessions.id_course, sessions.date_session, cohorts.name as linea, groups.name as grupo, courses.name as asignatura
+            FROM sessions
+            INNER JOIN groups ON groups.id = sessions.id_group
+            INNER JOIN cohorts on cohorts.id = groups.id_cohort
+            INNER JOIN courses ON courses.id = sessions.id_course
+            WHERE sessions.id_group = '".$grupo."'");
+
+        if($data != null) {
+            return $data;
+        }else{
+            return null;
+        }
+
+    }
+
+    public static function general($grupo, $curso){
+        $data = DB::select("select sessions.id_group, sessions.id_course, sessions.date_session, cohorts.name as linea, groups.name as grupo, courses.name as asignatura
+            FROM sessions
+            INNER JOIN groups ON groups.id = sessions.id_group
+            INNER JOIN cohorts on cohorts.id = groups.id_cohort
+            INNER JOIN courses ON courses.id = sessions.id_course
+            WHERE sessions.id_group = '".$grupo."'
+            AND sessions.id_course = '".$curso."'"
+            );
+
+        if($data != null) {
+            return $data;
+        }else{
+            return null;
+        }
+
+    }
+
 
     /**
      * Relacion con los  datos que se tiene de Session  

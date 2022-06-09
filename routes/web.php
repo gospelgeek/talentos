5eb135a5b1473c1cd6dd3012f850c3281fbcdc42
juliantello <95248642+juliantello@users.noreply.github.com>
2022-06-08 21:36:02 -0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -217,6 +217,7 @@ Route::get('pdfEstudiante/{id}', 'PdfsReportesController@PDF_estudiante')->name(
 Route::get('ver_asistencias/{id_moodle}/{id_grupo}', 'AsistenciasController@ver_asistencias')->name('estudiante.ver_asistencias');
 Route::post('cargar_asistencias', 'AsistenciasController@cargar_asistencias')->name('estudiante.cargar_asistencias');
 Route::get('detalles_sesiones/{id_student}/{id_course}', 'AsistenciasController@detalles')->name('estudiante.detalles_sesiones');
+Route::get('detalle_sesiones_ficha/{attendance_id}/{id_moodle}', 'AsistenciasController@detalle_sesiones_ficha')->name('estudiante.detalle_sesiones_ficha');
 
 //rutas condicione de salud
 Route::get('crear_condicion_salud', 'SocioEducativoController@crear_condicion')->name('crear_condicion_salud');

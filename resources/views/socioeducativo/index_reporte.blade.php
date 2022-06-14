8f4362a8dc1d5212ad94d315fea9e73399b0f2ab
StevenDavila <72047032+StevenDavila@users.noreply.github.com>
2022-06-14 16:30:30 -0500

    Update index_reporte.blade.php

--- resources/views/socioeducativo/index_reporte.blade.php
+++ resources/views/socioeducativo/index_reporte.blade.php
@@ -22,14 +22,14 @@
         <thead>
             <tr>
                 <td>Nombres</td>
-                <td>Nº Documento</td>
+                <td>N.Documento</td>
                 <td>Cohorte</td>
                 <td>Grupo</td>
                 <td>Clasificación</td>
-                <td>C. E</td>
-                <td>S. M</td>
-                <td>Prof. Acompañamiento</td>
-                <td>Cant. Seguimientos</td>
+                <td>C.E</td>
+                <td>S.M</td>
+                <td>Prof.Acompañamiento</td>
+                <td>N.Seguimientos</td>
                 <td id="r1">R.I</td>
                 <td>R.A</td>
                 <td>R.F</td>

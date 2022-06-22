af07548f9ad1df8774c7a49a9cc7554f25a6ca0f
StevenDavila <72047032+StevenDavila@users.noreply.github.com>
2022-06-21 20:53:49 -0500

    Update SocioEducationalFollowUp.php

--- app/SocioEducationalFollowUp.php
+++ app/SocioEducationalFollowUp.php
@@ -26,7 +26,7 @@ class SocioEducationalFollowUp extends Model
     //consulta para reporte
     public static function socioeducativo_reporte(){
 
-        $data = DB::select("select student_profile.id, student_profile.name, student_profile.lastname, student_profile.document_number, student_profile.id_state, student_groups.id_group as grupoid,  groups.name AS grupo, cohorts.name AS cohorte, assignment_students.id_user as id_profesional, users.name as nombre_profesional, users.apellidos_user as apellido_profersional, COUNT(student_profile.id) as cantidad_seguimientos, formalizations.acceptance_v1 as aceptacion2, formalizations.acceptance_v1 as aceptacion1, max(socio_educational_follow_ups.tracking_detail) as detalle, (SELECT health_conditions.special_requirements FROM health_conditions WHERE health_conditions.id_student = student_profile.id) as caso_especial, (SELECT health_conditions.mental_health FROM health_conditions WHERE health_conditions.id_student = student_profile.id) as salud_mental
+        $data = DB::select("select student_profile.id, student_profile.name, student_profile.lastname, student_profile.document_number, student_profile.id_state, student_groups.id_group as grupoid,  groups.name AS grupo, cohorts.name AS cohorte, assignment_students.id_user as id_profesional, users.name as nombre_profesional, users.apellidos_user as apellido_profersional, COUNT(student_profile.id) as cantidad_seguimientos, formalizations.acceptance_v1 as aceptacion2, formalizations.acceptance_v1 as aceptacion1, max(socio_educational_follow_ups.tracking_detail) as detalle, (SELECT health_conditions.special_requirements FROM health_conditions WHERE health_conditions.id_student = student_profile.id) as caso_especial, (SELECT health_conditions.mental_health FROM health_conditions WHERE health_conditions.id_student = student_profile.id) as salud_mental, conditions.name AS estado
             FROM student_profile 
             INNER JOIN student_groups ON student_groups.id_student = student_profile.id
             INNER JOIN groups ON groups.id = student_groups.id_group
@@ -35,6 +35,7 @@ class SocioEducationalFollowUp extends Model
             INNER JOIN users ON users.id = assignment_students.id_user
             INNER JOIN formalizations ON formalizations.id_student = student_profile.id
             INNER JOIN socio_educational_follow_ups ON socio_educational_follow_ups.id_student = student_profile.id
+            INNER JOIN conditions ON conditions.id = student_profile.id_state
             WHERE student_groups.deleted_at IS null
             AND socio_educational_follow_ups.deleted_at IS null
             AND assignment_students.deleted_at IS null

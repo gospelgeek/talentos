2fcfa2c5fdf3f35766ad9d961c08431c86963f38
StevenDavila <72047032+StevenDavila@users.noreply.github.com>
2022-06-29 16:55:17 -0500

    Update Formalization.php

--- app/Formalization.php
+++ app/Formalization.php
@@ -23,22 +23,35 @@ class Formalization extends Model
         'inscription_icfes',
         'presented_icfes',
         'observations',
+        'acceptance_date',
+        'acceptance_observation',
+        'returned_tablet',
+        'loan_tablet',
+        'serial_loan_tablet',
+        'observation_loan',
+        'loan_document_url',
+        'transfer_line2_to_line1',
     ];
     
     public static function formalizacion(){
 
-        $data = DB::select("select student_profile.id, student_profile.name, student_profile.lastname,student_profile.document_number,  
+        $data = DB::select("select student_profile.id, student_profile.name, student_profile.lastname,student_profile.document_number, student_profile.email, student_profile.cellphone,  
             student_groups.id_group as groupid, groups.name as grupo, cohorts.name as cohorte, 
             formalizations.acceptance_v2 as acceptance_v2, formalizations.tablets_v2 as tablets_v2,
             formalizations.serial_tablet as serial_tablet, formalizations.kit_date as kit_date, 
             formalizations.pre_registration_icfes as pre_registration_icfes, formalizations.inscription_icfes 
-            as inscription_icfes, formalizations.presented_icfes as presented_icfes,
-            (SELECT document_type.name FROM document_type WHERE document_type.id = student_profile.id_document_type) as tipodocumento
+            as inscription_icfes, formalizations.presented_icfes as presented_icfes, 
+            formalizations.acceptance_date as acceptance_date, formalizations.returned_tablet as returned_tablet, 
+            formalizations.loan_tablet as loan_tablet, formalizations.serial_loan_tablet as 
+            serial_loan_tablet, formalizations.loan_document_url as loan_document_url, formalizations.transfer_line2_to_line1 as cambio_linea,  
+            (SELECT document_type.name FROM document_type WHERE document_type.id = student_profile.id_document_type) as tipodocumento, conditions.name as estado,
+            (SELECT withdrawals.id_reasons FROM withdrawals WHERE withdrawals.id_student = student_profile.id) as motivoid
             FROM student_profile
             INNER JOIN student_groups ON student_groups.id_student = student_profile.id
             INNER JOIN groups ON groups.id = student_groups.id_group
             INNER JOIN cohorts on cohorts.id = groups.id_cohort
-            INNER JOIN formalizations ON formalizations.id_student = student_profile.id 
+            INNER JOIN formalizations ON formalizations.id_student = student_profile.id
+            INNER JOIN conditions ON conditions.id = student_profile.id_state 
             WHERE student_groups.deleted_at is null");
 
         if($data != null){

8c0562ed9604af29b63a92ecf580b0e3b54c0ff4
juliantello <95248642+juliantello@users.noreply.github.com>
2022-07-06 16:57:11 -0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -236,3 +236,6 @@ Route::delete('delete_apoyo_economico/{id}', 'FormalizacionController@apoyo_econ
 Route::get('icfes', 'IcfesController@index')->name('icfes');
 Route::get('datos_icfes', 'IcfesController@DatosIcfes')->name('datos_icfes');
 Route::get('resultado_area/{id_student}', 'IcfesController@resultadoArea')->name('resultado_icfes');
+
+//RUTAS CONTROLADOR SEGUIMIENTOS
+Route::get('reporte_notas/{linea}', 'SeguimientosController@reporte_notas')->name('estudiante.reporte_notas');

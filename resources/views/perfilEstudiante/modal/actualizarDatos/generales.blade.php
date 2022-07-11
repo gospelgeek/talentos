5fdf96d1252df4ab09d3df9952f8b804fd1aaf63
StevenDavila <72047032+StevenDavila@users.noreply.github.com>
2022-07-11 15:42:12 -0500

    Update generales.blade.php

--- resources/views/perfilEstudiante/modal/actualizarDatos/generales.blade.php
+++ resources/views/perfilEstudiante/modal/actualizarDatos/generales.blade.php
@@ -42,11 +42,15 @@
                       {!!Form::text('document_number', $verDatosPerfil ? $verDatosPerfil->document_number : null,['id'=>'numeroDocumento','class'=>'form-control','required','placeholder'=>'Numero documento'])!!}  
                       </div>
                       <div class="col-xs-12 col-sm-4 form-group {{$errors->has('body') ? 'has-errors' : ''}}">
-                      {!!Form::label('birth_date','Fecha nacimiento: ')!!}
-                      {!!Form::date('birth_date',$verDatosPerfil ? $verDatosPerfil->birth_date : null,['id'=>'fechaNacimiento', 'class'=>'form-control','placeholder'=>'Fecha nacimiento'])!!}   
+                      {!!Form::label('url_document_type','Enlace tipo documento: ')!!}
+                      {!!Form::text('url_document_type', $verDatosPerfil ? $verDatosPerfil->url_document_type : null,['id'=>'enlaceDocumento','class'=>'form-control','required','placeholder'=>'Enlace tipo documento'])!!}  
                       </div>
                   </div>
                   <div class="row">
+                    <div class="col-xs-12 col-sm-4 form-group {{$errors->has('body') ? 'has-errors' : ''}}">
+                      {!!Form::label('document_expedition_date','Fecha expedición documento: ')!!}
+                      {!!Form::date('document_expedition_date',$verDatosPerfil ? $verDatosPerfil->document_expedition_date : null,['id'=>'fechaExpedicion', 'class'=>'form-control','placeholder'=>'Fecha expedición documento'])!!}   
+                      </div>
                     <div class="col-xs-12 col-sm-4 form-group {{$errors->has('body') ? 'has-errors' : ''}}">
                       {!!Form::label('birth_date','Fecha nacimiento: ')!!}
                       {!!Form::date('birth_date',$verDatosPerfil ? $verDatosPerfil->birth_date : null,['id'=>'fechaNacimiento', 'class'=>'form-control','placeholder'=>'Fecha nacimiento'])!!}   
@@ -55,12 +59,12 @@
                       {!!Form::label('birthcity','Ciudad nacimiento: ')!!}
                       {!!Form::select('birthcity',$ciudad_nacimiento, $verDatosPerfil->birthcity ? $verDatosPerfil->birthcity->id : null,['id'=>'ciudadNacimiento','class'=>'form-control','required','placeholder'=>'Ciudad nacimiento'])!!}   
                       </div>
-                      <div class="col-xs-12 col-sm-4 form-group {{$errors->has('body') ? 'has-errors' : ''}}">
-                      {!!Form::label('email','Email: ')!!}
-                      {!!Form::email('email', $verDatosPerfil ? $verDatosPerfil->email : null,['id'=>'correo','class'=>'form-control','required','placeholder'=>'Correo electronico'])!!}  
-                      </div>
                   </div>
                   <div class="row">
+                    <div class="col-xs-12 col-sm-4 form-group {{$errors->has('body') ? 'has-errors' : ''}}">
+                      {!!Form::label('email','Email: ')!!}
+                      {!!Form::email('email', $verDatosPerfil ? $verDatosPerfil->email : null,['id'=>'correo','class'=>'form-control','required','placeholder'=>'Correo electronico'])!!}  
+                    </div>
                     <div class="col-xs-12 col-sm-4 form-group {{$errors->has('body') ? 'has-errors' : ''}}">
                       {!!Form::label('sex','Sexo: ')!!}
                       {!!Form::select('sexo',$sexo, $verDatosPerfil ? $verDatosPerfil->sex : null,['id'=>'sexo1','class'=>'form-control','required','placeholder'=>'Seleccionar sexo'])!!}  
@@ -69,12 +73,12 @@
                       {!!Form::label('gender','Genero: ')!!}
                       {!!Form::select('genero',$genero, $verDatosPerfil->gender ? $verDatosPerfil->gender->id : null,['id'=>'generoE','class'=>'form-control','required','placeholder'=>'Genero'])!!}  
                       </div>
-                      <div class="col-xs-12 col-sm-4 form-group {{$errors->has('body') ? 'has-errors' : ''}}">
+                  </div>
+                  <div class="row">
+                    <div class="col-xs-12 col-sm-4 form-group {{$errors->has('body') ? 'has-errors' : ''}}">
                       {!!Form::label('cellphone','Celular: ')!!}
                       {!!Form::text('cellphone', $verDatosPerfil ? $verDatosPerfil->cellphone : null,['id'=>'celular','class'=>'form-control','required','placeholder'=>'Celular'])!!}  
                       </div>
-                  </div>
-                  <div class="row">
                     <div class="col-xs-12 col-sm-4 form-group {{$errors->has('body') ? 'has-errors' : ''}}">
                       {!!Form::label('phone','Numero alternativo: ')!!}
                       {!!Form::text('phone', $verDatosPerfil ? $verDatosPerfil->phone : null,['id'=>'telefono','class'=>'form-control','required','placeholder'=>'Telefono'])!!}
@@ -83,30 +87,32 @@
                       {!!Form::label('direction','Direccion: ')!!}
                       {!!Form::text('direction', $verDatosPerfil ? $verDatosPerfil->direction : null,['id'=>'direccion12','class'=>'form-control','required','placeholder'=>'Direccion'])!!}  
                       </div>
-                      <div class="col-xs-12 col-sm-4 form-group {{$errors->has('body') ? 'has-errors' : ''}}">
+                  </div>
+                  <div class="row">
+                    <div class="col-xs-12 col-sm-4 form-group {{$errors->has('body') ? 'has-errors' : ''}}">
                       {!!Form::label('neighborhood','Barrio recidencia: ')!!}
                       {!!Form::select('barrio',$barrio, $verDatosPerfil->neighborhood ? $verDatosPerfil->neighborhood->id : null,['id'=>'barrioV','class'=>'form-control','required','placeholder'=>'Genero'])!!}  
                       </div>
+                    <div class="col-xs-12 col-sm-4 form-group {{$errors->has('body') ? 'has-errors' : ''}}">
+                      {!!Form::label('emergency_contact','Numero contacto emergencia: ')!!}
+                      {!!Form::text('emergency_contact', $verDatosPerfil ? $verDatosPerfil->emergency_contact : null,['id'=>'emergencia','class'=>'form-control','required','placeholder'=>'Contacto emergencia'])!!}
+                    </div>
+                    <div class="col-xs-12 col-sm-4 form-group {{$errors->has('body') ? 'has-errors' : ''}}">
+                      {!!Form::label('emergency_contact_name','Nombre contacto de emergencia: ')!!}
+                      {!!Form::text('emergency_contact_name', $verDatosPerfil ? $verDatosPerfil->emergency_contact_name : null,['id'=>'emergencia_nombre','class'=>'form-control','required','placeholder'=>'Nombre contacto emergencia'])!!}
+                    </div>
                   </div>
                   <div class="row">
+                    <div class="col-xs-12 col-sm-4 form-group {{$errors->has('body') ? 'has-errors' : ''}}">
+                      {!!Form::label('relationship','Parentezco: ')!!}
+                      {!!Form::text('relationship', $verDatosPerfil ? $verDatosPerfil->relationship : null,['id'=>'parentezco','class'=>'form-control','required','placeholder'=>'Parentezco'])!!}
+                    </div>
                     @if(auth()->user()->rol_id == 1)
                     <div class="col-xs-12 col-sm-4 form-group {{$errors->has('body') ? 'has-errors' : ''}}">
                       {!!Form::label('student_code','Codigo estudiante: ')!!}
                       {!!Form::text('student_code', $verDatosPerfil ? $verDatosPerfil->student_code : null,['id'=>'codEstu','class'=>'form-control','required','placeholder'=>'Codigo estudiante'])!!}  
                     </div>
                     @endif
-                    <div class="col-xs-12 col-sm-4 form-group {{$errors->has('body') ? 'has-errors' : ''}}">
-                      {!!Form::label('emergency_contact_name','Nombre contacto de emergencia: ')!!}
-                      {!!Form::text('emergency_contact_name', $verDatosPerfil ? $verDatosPerfil->emergency_contact_name : null,['id'=>'emergencia_nombre','class'=>'form-control','required','placeholder'=>'Nombre contacto emergencia'])!!}
-                    </div>
-                    <div class="col-xs-12 col-sm-4 form-group {{$errors->has('body') ? 'has-errors' : ''}}">
-                      {!!Form::label('relationship','Parentezco: ')!!}
-                      {!!Form::text('relationship', $verDatosPerfil ? $verDatosPerfil->relationship : null,['id'=>'parentezco','class'=>'form-control','required','placeholder'=>'Parentezco'])!!}
-                    </div>
-                    <div class="col-xs-12 col-sm-4 form-group {{$errors->has('body') ? 'has-errors' : ''}}">
-                      {!!Form::label('emergency_contact','Numero de contacto de emergencia: ')!!}
-                      {!!Form::text('emergency_contact', $verDatosPerfil ? $verDatosPerfil->emergency_contact : null,['id'=>'emergencia','class'=>'form-control','required','placeholder'=>'Contacto emergencia'])!!}
-                    </div>
                   </div>
                 </div>
               </div>

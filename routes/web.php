bfe0423809b992cd2fc5682e84ecef2501aeddef
Juan Sebastian Gomez Mezu <62127725+kingomez21@users.noreply.github.com>
2022-07-16 12:54:30 -0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -221,3 +221,5 @@ Route::delete('delete_apoyo_economico/{id}', 'FormalizacionController@apoyo_econ
 
 Route::get('icfes', 'IcfesController@index')->name('icfes');
 Route::get('datos_icfes', 'IcfesController@DatosIcfes')->name('datos_icfes');
+Route::post('descarga_certificado/', 'CertificadoController@certificado')->name('pdf.calificacion');
+Route::get('certificado/', 'CertificadoController@index')->name('descarga.certificado');

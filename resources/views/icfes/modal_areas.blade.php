42a89a0c5d59cc997eaaf14f72db1125dd24dcc6
Juan Sebastian Gomez Mezu <62127725+kingomez21@users.noreply.github.com>
2022-07-26 22:21:18 -0500

    Add files via upload

---
+++ resources/views/icfes/modal_areas.blade.php
@@ -0,0 +1,43 @@
+<div class="modal" data-refresh="true"  tabindex="-1" id="modal-areas">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+      <div id="recargar" class="modal-body">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 id="nombre" style="text-align:center;font-size : 30px;font-weight: bolder;"></h4>
+        <div  class="table-responsive">
+        <table id="pruebaAreas" class="table table-bordered table-striped">
+          <caption id="mensaje" style="caption-side: top;text-align:center;">RESULTADO POR AREA</caption>
+          <thead>
+            <td>Area</td>
+            <td>Calificacion</td>
+          </thead>
+        </table>
+      </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<!--
+<div class="modal fade" id="modal-areas" tabindex="-1" data-refresh="true"  aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Modal ejemplo</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <h1>jkjk</h1>
+            </div>
+            <div class="modal-footer">
+                <h1>mod2</h1>
+            </div>
+        </div>
+    </div>
+</div>-->

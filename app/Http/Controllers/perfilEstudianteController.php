9c3b38645b26e5cef7fbee44ec94332f98603d11
Juan Sebastian Gomez Mezu <62127725+kingomez21@users.noreply.github.com>
2022-07-30 11:15:23 -0500

    Update perfilEstudianteController.php

--- app/Http/Controllers/perfilEstudianteController.php
+++ app/Http/Controllers/perfilEstudianteController.php
@@ -247,7 +247,7 @@ class perfilEstudianteController extends Controller
 
     public function verPerfilEstudiante($id)
     {
-
+        $iden = $id;
         //return $id;
         /* $user = auth()->user();
         /*if($user['rol_id'] == 6){
@@ -259,11 +259,60 @@ class perfilEstudianteController extends Controller
                 return Redirect::to('/estudiante');
             }           
         }*/
+        
+        $totalS1 = DB::select("SELECT icfes_students.total_score as total1 
+        FROM icfes_students WHERE icfes_students.id_student = ? 
+        AND icfes_students.id_icfes_test = 1", [$id]);
 
+        $totalS2 = DB::select("SELECT icfes_students.total_score as total2 
+        FROM icfes_students WHERE icfes_students.id_student = ? 
+        AND icfes_students.id_icfes_test = 2", [$id]);
+
+        $t1 = 0;
+        $t2 = 0;
+
+        if($totalS1 == []){
+            $t1 = 0;
+        }else {
+            $t1 = $totalS1[0]->total1;
+        }
+
+        if($totalS2 == []){
+            //dd($t2);
+            $t2 = 0;
+        }else {
+            $t2 = $totalS2[0]->total2;
+        }
+
+        $totalSimulacros = $t1 + $t2;
+        
+        $url_entrada = DB::select("SELECT icfes_students.url_support as url FROM 
+        icfes_students WHERE id_icfes_test = 4 AND id_student = ?", [$id]);
+
+        $url_salida = DB::select("SELECT icfes_students.url_support as url FROM 
+        icfes_students WHERE id_icfes_test = 5 AND id_student = ?", [$id]);
+
+        $pruebaS1 = DB::select("SELECT id_icfes_test as prueba FROM icfes_students WHERE id_icfes_test = 1 AND id_student = ?", [$id]);
+        $pruebaS2 = DB::select("SELECT id_icfes_test as prueba FROM icfes_students WHERE id_icfes_test = 2 AND id_student = ?", [$id]);
+        $pruebaS3 = DB::select("SELECT id_icfes_test as prueba FROM icfes_students WHERE id_icfes_test = 3 AND id_student = ?", [$id]);
+        $pruebaS4 = DB::select("SELECT id_icfes_test as prueba FROM icfes_students WHERE id_icfes_test = 4 AND id_student = ?", [$id]);
+        $pruebaS5 = DB::select("SELECT id_icfes_test as prueba FROM icfes_students WHERE id_icfes_test = 5 AND id_student = ?", [$id]);
+
+        
         $verDatosPerfil = perfilEstudiante::findOrFail($id);
         //dd($verDatosPerfil);
         $asignacion = AssignmentStudent::where('id_student', $id)->first();
         $cohort = $verDatosPerfil->studentGroup->group->cohort->id;
+        
+        $variacion = 0;
+
+        if($cohort == 1 || $cohort == 2){
+            $dataIcfesEn = DB::select("SELECT icfes_students.total_score as puntajeEntrada FROM 
+                icfes_students WHERE id_icfes_test = 4 AND id_student = ?", [$id]);
+            if($dataIcfesEn == []){ $variacion = 0;}else{$variacion = $dataIcfesEn[0]->puntajeEntrada;};
+            
+        }
+        
         $grupos = Group::where('id_cohort', $cohort)->pluck('name', 'id');
         //return $grupos;
 
@@ -345,7 +394,7 @@ class perfilEstudianteController extends Controller
             $foto = $foto[5];
         }
 
-        return view('perfilEstudiante.verDatos', compact('motivos', 'foto', 'estado', 'verDatosPerfil', 'genero', 'sexo', 'tipo_documento', 'documento', 'edad', 'ciudad_nacimiento', 'barrio', 'ocupacion', 'estado_civil', 'residencia', 'vivienda', 'regimen', 'condicion', 'discapacidad', 'etnia', 'estado', 'beneficios', 'seguimientos', 'cohorte', 'grupos', 'asignacion', 'apoyo_economico'));
+        return view('perfilEstudiante.verDatos', compact('motivos', 'foto', 'estado', 'verDatosPerfil', 'genero', 'sexo', 'tipo_documento', 'documento', 'edad', 'ciudad_nacimiento', 'barrio', 'ocupacion', 'estado_civil', 'residencia', 'vivienda', 'regimen', 'condicion', 'discapacidad', 'etnia', 'estado', 'beneficios', 'seguimientos', 'cohorte', 'grupos', 'asignacion', 'apoyo_economico', 'iden', 't1', 't2', 'totalSimulacros', 'url_entrada', 'url_salida', 'pruebaS1', 'pruebaS2', 'pruebaS3', 'pruebaS4', 'pruebaS5', 'variacion'));
     }
 
     public function verDatosSocieconomicos($id)

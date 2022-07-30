f017ff85ef54672217994673f27ee4a89c059060
Juan Sebastian Gomez Mezu <62127725+kingomez21@users.noreply.github.com>
2022-07-30 11:26:03 -0500

    Update filtroestudiantes.js

--- public/js/filtroestudiantes.js
+++ public/js/filtroestudiantes.js
@@ -414,6 +414,39 @@ $(".accordion-titulo-5").click(function(e){
                     
         }
 });
+
+$(".accordion-titulo-7").click(function(e){
+        
+  e.preventDefault();
+
+  var contenido=$(this).next(".accordion-content-7");
+
+
+  if(contenido.css("display")=="none"){ //open        
+    contenido.slideDown(250);         
+    $(this).addClass("open");
+    $("#contenido-1").hide();
+    $("#titulo-1").removeClass("open");
+    $("#contenido-2").hide();
+    $("#titulo-2").removeClass("open");
+    $("#contenido-3").hide();
+    $("#titulo-3").removeClass("open");
+    $("#contenido-4").hide();
+    $("#titulo-4").removeClass("open");
+    //$("#contenido-5").hide();
+    //$("#titulo-5").removeClass("open");
+    $("#contenido-6").hide();
+    $("#titulo-6").removeClass("open"); 
+      
+  }
+  else{ //close
+
+    contenido.slideUp(250);
+    $(this).removeClass("open");
+              
+  }
+});
+
 function abrirmodal(id){
 
   $('#sesiones').empty();

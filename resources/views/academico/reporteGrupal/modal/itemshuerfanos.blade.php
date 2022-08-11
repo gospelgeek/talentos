bc70102ec9c707276f11245d423917e459b188e4
juliantello <95248642+juliantello@users.noreply.github.com>
2022-08-11 15:25:32 -0500

    Update itemshuerfanos.blade.php

--- resources/views/academico/reporteGrupal/modal/itemshuerfanos.blade.php
+++ resources/views/academico/reporteGrupal/modal/itemshuerfanos.blade.php
@@ -1,18 +1,22 @@
 <div class="modal" data-refresh="true"  tabindex="-1" id="modal_items_huerfanos">
-  <div class="modal-dialog modal-lg">
+  <div class="modal-dialog modal-md">    
     <div class="modal-content">
-      <div id="recargar" class="modal-body">
+      <div class="modal-header">
+        <h4 class="modal-title pull-center" style="justify-content: center"><Strong id="title">SEGUIMIENTOS ACADEMICOS</strong></h4>
         <button type="button" class="close" data-dismiss="modal">&times;</button>
-        <h4 id="nombre" style="text-align:center;font-size : 30px;font-weight: bolder;"></h4>
-        <div  class="table-responsive">
-        <table id="example2" class="table table-bordered table-striped">
-          <caption id="curso" style="caption-side: top;text-align:center;"></caption>
-          <thead>
-            <td>Item</td>
-            <td>Calificacion</td>
-          </thead>
-        </table>
       </div>
+      <div class="modal-body">
+        <h4 id="nombre" style="text-align:center;font-size : 20px;font-weight: bolder;"></h4>
+        <h4 id="curso" style="text-align:center;font-size : 15px; font-weight: bolder;"></h4>
+        <h4 id="categoria" style="text-align:center;font-size : 15px; font-weight: bolder;"></h4>
+        <div  class="table-responsive">
+          <table id="example2" class="table table-bordered table-striped">
+            <thead>
+              <td>Item</td>
+              <td>Calificacion</td>
+            </thead>
+          </table>
+        </div>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
@@ -20,5 +24,3 @@
     </div>
   </div>
 </div>
-
-                        

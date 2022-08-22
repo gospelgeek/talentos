b7b7ca4cd09593b31aebe02944d54b166370dd27
StevenDavila <72047032+StevenDavila@users.noreply.github.com>
2022-08-22 16:19:41 -0500

    Update Withdrawals.php

--- app/Withdrawals.php
+++ app/Withdrawals.php
@@ -39,6 +39,182 @@ class Withdrawals extends Model
         }
     }
     //
+    
+    public static function febrero(){
+        $data = DB::select("select student_profile.id ,student_profile.name, student_profile.lastname, student_profile.id_document_type, (SELECT document_type.name FROM document_type WHERE document_type.id = student_profile.id_document_type) as tipodocumento, student_profile.document_number, student_groups.id_group as groupid, groups.name as grupo, cohorts.name as cohorte, assignment_students.id_user as id_profesional, CONCAT(users.name,' ', users.apellidos_user) encargado, conditions.name as condicion, withdrawals.id_reasons as id_motivo, reasons.name as motivo, withdrawals.fecha as fecha, withdrawals.observation as observacion, withdrawals.url as url, withdrawals.created_at
+            FROM student_profile
+            INNER JOIN student_groups ON student_groups.id_student = student_profile.id
+            INNER JOIN groups ON groups.id = student_groups.id_group
+            INNER JOIN cohorts on cohorts.id = groups.id_cohort
+            INNER JOIN assignment_students ON assignment_students.id_student = student_profile.id
+            INNER JOIN conditions ON conditions.id = student_profile.id_state
+            INNER JOIN users ON users.id = assignment_students.id_user
+            INNER JOIN withdrawals ON withdrawals.id_student = student_profile.id 
+            INNER JOIN reasons ON reasons.id = withdrawals.id_reasons
+            WHERE student_groups.deleted_at IS null
+            AND assignment_students.deleted_at IS null
+            AND MONTH(withdrawals.created_at) = 2");
+
+        if($data != null){
+            return $data;
+        }else{
+            return null;
+        }
+    }
+
+    public static function marzo(){
+        $data = DB::select("select student_profile.id ,student_profile.name, student_profile.lastname, student_profile.id_document_type, (SELECT document_type.name FROM document_type WHERE document_type.id = student_profile.id_document_type) as tipodocumento, student_profile.document_number, student_groups.id_group as groupid, groups.name as grupo, cohorts.name as cohorte, assignment_students.id_user as id_profesional, CONCAT(users.name,' ', users.apellidos_user) encargado, conditions.name as condicion, withdrawals.id_reasons as id_motivo, reasons.name as motivo, withdrawals.fecha as fecha, withdrawals.observation as observacion, withdrawals.url as url, withdrawals.created_at
+            FROM student_profile
+            INNER JOIN student_groups ON student_groups.id_student = student_profile.id
+            INNER JOIN groups ON groups.id = student_groups.id_group
+            INNER JOIN cohorts on cohorts.id = groups.id_cohort
+            INNER JOIN assignment_students ON assignment_students.id_student = student_profile.id
+            INNER JOIN conditions ON conditions.id = student_profile.id_state
+            INNER JOIN users ON users.id = assignment_students.id_user
+            INNER JOIN withdrawals ON withdrawals.id_student = student_profile.id 
+            INNER JOIN reasons ON reasons.id = withdrawals.id_reasons
+            WHERE student_groups.deleted_at IS null
+            AND assignment_students.deleted_at IS null
+            AND MONTH(withdrawals.created_at) = 3");
+
+        if($data != null){
+            return $data;
+        }else{
+            return null;
+        }
+    }
+
+    public static function abril(){
+        $data = DB::select("select student_profile.id ,student_profile.name, student_profile.lastname, student_profile.id_document_type, (SELECT document_type.name FROM document_type WHERE document_type.id = student_profile.id_document_type) as tipodocumento, student_profile.document_number, student_groups.id_group as groupid, groups.name as grupo, cohorts.name as cohorte, assignment_students.id_user as id_profesional, CONCAT(users.name,' ', users.apellidos_user) encargado, conditions.name as condicion, withdrawals.id_reasons as id_motivo, reasons.name as motivo, withdrawals.fecha as fecha, withdrawals.observation as observacion, withdrawals.url as url, withdrawals.created_at
+            FROM student_profile
+            INNER JOIN student_groups ON student_groups.id_student = student_profile.id
+            INNER JOIN groups ON groups.id = student_groups.id_group
+            INNER JOIN cohorts on cohorts.id = groups.id_cohort
+            INNER JOIN assignment_students ON assignment_students.id_student = student_profile.id
+            INNER JOIN conditions ON conditions.id = student_profile.id_state
+            INNER JOIN users ON users.id = assignment_students.id_user
+            INNER JOIN withdrawals ON withdrawals.id_student = student_profile.id 
+            INNER JOIN reasons ON reasons.id = withdrawals.id_reasons
+            WHERE student_groups.deleted_at IS null
+            AND assignment_students.deleted_at IS null
+            AND MONTH(withdrawals.created_at) = 4");
+
+        if($data != null){
+            return $data;
+        }else{
+            return null;
+        }
+    }
+
+    public static function mayo(){
+        $data = DB::select("select student_profile.id ,student_profile.name, student_profile.lastname, student_profile.id_document_type, (SELECT document_type.name FROM document_type WHERE document_type.id = student_profile.id_document_type) as tipodocumento, student_profile.document_number, student_groups.id_group as groupid, groups.name as grupo, cohorts.name as cohorte, assignment_students.id_user as id_profesional, CONCAT(users.name,' ', users.apellidos_user) encargado, conditions.name as condicion, withdrawals.id_reasons as id_motivo, reasons.name as motivo, withdrawals.fecha as fecha, withdrawals.observation as observacion, withdrawals.url as url, withdrawals.created_at
+            FROM student_profile
+            INNER JOIN student_groups ON student_groups.id_student = student_profile.id
+            INNER JOIN groups ON groups.id = student_groups.id_group
+            INNER JOIN cohorts on cohorts.id = groups.id_cohort
+            INNER JOIN assignment_students ON assignment_students.id_student = student_profile.id
+            INNER JOIN conditions ON conditions.id = student_profile.id_state
+            INNER JOIN users ON users.id = assignment_students.id_user
+            INNER JOIN withdrawals ON withdrawals.id_student = student_profile.id 
+            INNER JOIN reasons ON reasons.id = withdrawals.id_reasons
+            WHERE student_groups.deleted_at IS null
+            AND assignment_students.deleted_at IS null
+            AND MONTH(withdrawals.created_at) = 5");
+
+        if($data != null){
+            return $data;
+        }else{
+            return null;
+        }
+    }
+
+    public static function junio(){
+        $data = DB::select("select student_profile.id ,student_profile.name, student_profile.lastname, student_profile.id_document_type, (SELECT document_type.name FROM document_type WHERE document_type.id = student_profile.id_document_type) as tipodocumento, student_profile.document_number, student_groups.id_group as groupid, groups.name as grupo, cohorts.name as cohorte, assignment_students.id_user as id_profesional, CONCAT(users.name,' ', users.apellidos_user) encargado, conditions.name as condicion, withdrawals.id_reasons as id_motivo, reasons.name as motivo, withdrawals.fecha as fecha, withdrawals.observation as observacion, withdrawals.url as url, withdrawals.created_at
+            FROM student_profile
+            INNER JOIN student_groups ON student_groups.id_student = student_profile.id
+            INNER JOIN groups ON groups.id = student_groups.id_group
+            INNER JOIN cohorts on cohorts.id = groups.id_cohort
+            INNER JOIN assignment_students ON assignment_students.id_student = student_profile.id
+            INNER JOIN conditions ON conditions.id = student_profile.id_state
+            INNER JOIN users ON users.id = assignment_students.id_user
+            INNER JOIN withdrawals ON withdrawals.id_student = student_profile.id 
+            INNER JOIN reasons ON reasons.id = withdrawals.id_reasons
+            WHERE student_groups.deleted_at IS null
+            AND assignment_students.deleted_at IS null
+            AND MONTH(withdrawals.created_at) = 6");
+
+        if($data != null){
+            return $data;
+        }else{
+            return null;
+        }
+    }
+
+    public static function julio(){
+        $data = DB::select("select student_profile.id ,student_profile.name, student_profile.lastname, student_profile.id_document_type, (SELECT document_type.name FROM document_type WHERE document_type.id = student_profile.id_document_type) as tipodocumento, student_profile.document_number, student_groups.id_group as groupid, groups.name as grupo, cohorts.name as cohorte, assignment_students.id_user as id_profesional, CONCAT(users.name,' ', users.apellidos_user) encargado, conditions.name as condicion, withdrawals.id_reasons as id_motivo, reasons.name as motivo, withdrawals.fecha as fecha, withdrawals.observation as observacion, withdrawals.url as url, withdrawals.created_at
+            FROM student_profile
+            INNER JOIN student_groups ON student_groups.id_student = student_profile.id
+            INNER JOIN groups ON groups.id = student_groups.id_group
+            INNER JOIN cohorts on cohorts.id = groups.id_cohort
+            INNER JOIN assignment_students ON assignment_students.id_student = student_profile.id
+            INNER JOIN conditions ON conditions.id = student_profile.id_state
+            INNER JOIN users ON users.id = assignment_students.id_user
+            INNER JOIN withdrawals ON withdrawals.id_student = student_profile.id 
+            INNER JOIN reasons ON reasons.id = withdrawals.id_reasons
+            WHERE student_groups.deleted_at IS null
+            AND assignment_students.deleted_at IS null
+            AND MONTH(withdrawals.created_at) = 7");
+
+        if($data != null){
+            return $data;
+        }else{
+            return null;
+        }
+    }
+
+    public static function agosto(){
+        $data = DB::select("select student_profile.id ,student_profile.name, student_profile.lastname, student_profile.id_document_type, (SELECT document_type.name FROM document_type WHERE document_type.id = student_profile.id_document_type) as tipodocumento, student_profile.document_number, student_groups.id_group as groupid, groups.name as grupo, cohorts.name as cohorte, assignment_students.id_user as id_profesional, CONCAT(users.name,' ', users.apellidos_user) encargado, conditions.name as condicion, withdrawals.id_reasons as id_motivo, reasons.name as motivo, withdrawals.fecha as fecha, withdrawals.observation as observacion, withdrawals.url as url, withdrawals.created_at
+            FROM student_profile
+            INNER JOIN student_groups ON student_groups.id_student = student_profile.id
+            INNER JOIN groups ON groups.id = student_groups.id_group
+            INNER JOIN cohorts on cohorts.id = groups.id_cohort
+            INNER JOIN assignment_students ON assignment_students.id_student = student_profile.id
+            INNER JOIN conditions ON conditions.id = student_profile.id_state
+            INNER JOIN users ON users.id = assignment_students.id_user
+            INNER JOIN withdrawals ON withdrawals.id_student = student_profile.id 
+            INNER JOIN reasons ON reasons.id = withdrawals.id_reasons
+            WHERE student_groups.deleted_at IS null
+            AND assignment_students.deleted_at IS null
+            AND MONTH(withdrawals.created_at) = 8");
+
+        if($data != null){
+            return $data;
+        }else{
+            return null;
+        }
+    }
+
+    /*public static function septiembre(){
+        $data = DB::select("select student_profile.id ,student_profile.name, student_profile.lastname, student_profile.id_document_type, (SELECT document_type.name FROM document_type WHERE document_type.id = student_profile.id_document_type) as tipodocumento, student_profile.document_number, student_groups.id_group as groupid, groups.name as grupo, cohorts.name as cohorte, assignment_students.id_user as id_profesional, CONCAT(users.name,' ', users.apellidos_user) encargado, conditions.name as condicion, withdrawals.id_reasons as id_motivo, reasons.name as motivo, withdrawals.fecha as fecha, withdrawals.observation as observacion, withdrawals.url as url, withdrawals.created_at
+            FROM student_profile
+            INNER JOIN student_groups ON student_groups.id_student = student_profile.id
+            INNER JOIN groups ON groups.id = student_groups.id_group
+            INNER JOIN cohorts on cohorts.id = groups.id_cohort
+            INNER JOIN assignment_students ON assignment_students.id_student = student_profile.id
+            INNER JOIN conditions ON conditions.id = student_profile.id_state
+            INNER JOIN users ON users.id = assignment_students.id_user
+            INNER JOIN withdrawals ON withdrawals.id_student = student_profile.id 
+            INNER JOIN reasons ON reasons.id = withdrawals.id_reasons
+            WHERE student_groups.deleted_at IS null
+            AND assignment_students.deleted_at IS null
+            AND MONTH(withdrawals.created_at) = 9");
+
+        if($data != null){
+            return $data;
+        }else{
+            return null;
+        }
+    }*/
     /**
      * Relacion con los  datos que se tiene de Withdrawals  
      * con la tabla Reasons

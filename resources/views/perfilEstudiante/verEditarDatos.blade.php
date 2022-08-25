dc11e93aa2b2e95449e830ccf2cb025903927c2d
StevenDavila <72047032+StevenDavila@users.noreply.github.com>
2022-08-25 10:31:49 -0500

    Update verEditarDatos.blade.php

--- resources/views/perfilEstudiante/verEditarDatos.blade.php
+++ resources/views/perfilEstudiante/verEditarDatos.blade.php
@@ -209,7 +209,7 @@
 					<div class="col-xs-2 col-md-2">
 						<div class="row">
 							<div class="col-xs-12 col-md-12">
-								<input readonly class="form-control" type="text" name="departamento_nacimiento" id="departamento_nacimiento" value="{{ old('departamento_nacimiento', $verDatosPerfil->id_birth_department ?  $verDatosPerfil->birthcity->birthdepartament->name : null) }}">
+								<input readonly class="form-control" type="text" name="departamento_nacimiento" id="departamento_nacimiento" value="{{ old('departamento_nacimiento', $verDatosPerfil->birthdepartament ?  $verDatosPerfil->birthcity->birthdepartament->name : null) }}">
 							</div>
 						</div>                	
             		</div>

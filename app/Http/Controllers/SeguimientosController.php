0803949b01f8abf6aac0d367128fe2e21b238f0e
StevenDavila <72047032+StevenDavila@users.noreply.github.com>
2022-08-29 15:08:31 -0500

    Update SeguimientosController.php

--- app/Http/Controllers/SeguimientosController.php
+++ app/Http/Controllers/SeguimientosController.php
@@ -873,7 +873,7 @@ public function __construct()
                                 case 'FISICA':
                                     if ((strpos($cursos->category_name, 'Asistencia') !== false) || (strpos($cursos->category_name, 'asistencia') !== false) || (strpos($cursos->category_name, 'ASISTENCIA') !== false)) {
                                         
-                                        $fisica_asistencia = $cursos->grade;
+                                        $filosofia_asistencias = $cursos->grade;
                                     }
 
                                     if ((strpos($cursos->category_name, 'Actividades') !== false) || (strpos($cursos->category_name,'ACTIVIDADES') !== false) || (strpos($cursos->category_name, 'COMPONENTE') !== false) || (strpos($cursos->category_name, 'SEGUIMIENTO') !== false) || (strpos($cursos->category_name, 'Seguimiento') !== false)) {

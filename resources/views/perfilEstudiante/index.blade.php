f8efc27f1f5cf6c5a889ef02d91799f4ad710453
StevenDavila <72047032+StevenDavila@users.noreply.github.com>
2022-10-10 13:53:07 -0500

    Update index.blade.php

--- resources/views/perfilEstudiante/index.blade.php
+++ resources/views/perfilEstudiante/index.blade.php
@@ -212,15 +212,15 @@
         "columns": [
             {data: 'name'},
             {data: 'cant_linea_1', render:function(data, type, row, meta){
-                    return data;
+                    return parseFloat(data);
                 }
             },
             {data: 'cant_linea_2', render:function(data, type, row, meta){
-                    return data;
+                    return return parseFloat(data);
                 }
             },
             {data: 'cant_linea_3', render:function(data, type, row, meta){
-                    return data;
+                    return return parseFloat(data);
                 }
             },
         ],

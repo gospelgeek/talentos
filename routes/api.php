19cd2fa0af38f899b76c8b75876dcc1671eaa068
StevenDavila <72047032+StevenDavila@users.noreply.github.com>
2022-10-13 13:38:18 -0500

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,14 @@ use Illuminate\Support\Facades\Route;
     return $request->user();
 });*/
 
+//Datos generales
 Route::resource('datos_api/{user}/{token}/', 'api\DatosApiController');
+//reporte notas
 Route::get('reporte_notas_linea_1/{user}/{token}/', 'api\DatosApiController@notas_linea_1')->name('reporte_notas_linea_1');
 Route::get('reporte_notas_linea_2/{user}/{token}/', 'api\DatosApiController@notas_linea_2')->name('reporte_notas_linea_2');
 Route::get('reporte_notas_linea_3/{user}/{token}/', 'api\DatosApiController@notas_linea_3')->name('reporte_notas_linea_3');
+//comparativo icfes
+Route::get('reporte_comparativo_icfes_l1/{user}/{token}/', 'api\DatosApiController@comparativo_icfes_l1')->name('reporte_comparativo_icfes_l1');
+Route::get('reporte_comparativo_icfes_l2/{user}/{token}/', 'api\DatosApiController@comparativo_icfes_l2')->name('reporte_comparativo_icfes_l2');
+Route::get('reporte_comparativo_icfes_l3/{user}/{token}/', 'api\DatosApiController@comparativo_icfes_l3')->name('reporte_comparativo_icfes_l3');
+

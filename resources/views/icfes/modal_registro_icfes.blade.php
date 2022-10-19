2eedecd306da74c3e14fe6ee2ff1787edc334b63
Juan Sebastian Gomez Mezu <62127725+kingomez21@users.noreply.github.com>
2022-10-18 19:05:13 -0500

    Update modal_registro_icfes.blade.php

--- resources/views/icfes/modal_registro_icfes.blade.php
+++ resources/views/icfes/modal_registro_icfes.blade.php
@@ -1,7 +1,8 @@
 <div class="modal" data-refresh="true" tabindex="-1" id="modal-registro">
   <div class="modal-dialog modal-lg">
     <div class="modal-content">
-      <form action="/registro_icfes" method="post">
+      <form id="_registro">
+
         @csrf
         <div id="recargar" class="modal-body">
           <button type="button" class="close" data-dismiss="modal">&times;</button>
@@ -12,28 +13,35 @@
             <div class="form-group">
               <div class="row">
                 <div class="col-md-6">
-                  <label for="">INGRESE LA IDENTIFICACION DEL ESTUDIANTE</label>
-                  <input class="form-control" type="text" name="identificacion" id="identificacion">
+                  <p>Ingrese el nombre  del estudiante</p>
+                  <input class="form-control" list="estudiantes" required type="text" name="identificacion" id="identificacion">
+                  <datalist id="estudiantes">
+                    @foreach ($estudiantes as $lista)
+                    <option value="{{$lista->name}} {{$lista->lastname}}-{{$lista->document_number}}">{{$lista->name}} {{$lista->lastname}}</option>
+                    @endforeach
+                  </datalist>
                 </div>
                 <div class="col-md-6">
-                  <label for="">INGRESE EL PUNTAJE</label>
-                  <input class="form-control" type="number" name="puntaje" id="puntaje">
+                  <p>Ingrese el Puntaje total</p>
+                  <input class="form-control" type="number" min="0" required max="500" name="puntaje" id="puntaje">
                 </div>
               </div>
 
               <div>
-                <label for="">SELECCIONE LA PRUEBA </label>
-                <select class="form-control" name="prueba" id="prueba">
-                  <option selected value="--">--------------</option>
-                  @foreach($pruebas as $data)
-                  <option value="{{$data->id}}">{{$data->name}}</option>
-                  @endforeach
+                <p>Seleccione la Prueba</p>
+                <select class="form-control" disabled>
+                  <option selected value="{{$pruebas[4]->id}}">{{$pruebas[4]->name}}</option>
+
+                </select>
+                <select class="form-control" name="prueba" id="prueba" hidden>
+                  <option selected value="{{$pruebas[4]->id}}">{{$pruebas[4]->name}}</option>
+
                 </select>
               </div>
 
               <div>
-                <label for="">INGRESE EL SOPORTE URL </label>
-                <input class="form-control" type="text" name="url" id="url">
+                <p>Ingrese el Soporte de URL</p>
+                <input class="form-control" required type="text" name="url" id="url">
               </div>
             </div>
 
@@ -45,45 +53,105 @@
               &nbsp;
               &nbsp;
               <div class="col-ms-4">
-                <input class="form-control mt-0" style="width: 20px; height: 19px;" type="checkbox" name="r_areas" id="r_areas">
+                <input class="form-control mt-0" checked style="width: 20px; height: 19px;" type="checkbox" name="r_areas" id="r_areas">
               </div>
             </div>
             <br>
 
             <div id="form_areas" hidden class="form-group">
+
               <div class="row">
+
                 <div class="col-md-6">
-                  <label for="">INGRESE LA CALIFICACION DE LECTURA CRITICA</label>
-                  <input class="form-control"  type="text" name="lecturaC" id="lecturaC">
+                  <div class="row">
+
+                    <div class="col-md-6">
+                      <p>Lectura Critica:</p>
+                    </div>
+
+                    <div class="col-md-4">
+                      <input class="form-control" type="number" name="lecturaC" id="lecturaC">
+                    </div>
+
+                  </div>
                 </div>
+                
                 <div class="col-md-6">
-                  <label for="">INGRESE LA CALIFICACION DE MATEMATICAS</label>
-                  <input class="form-control"  type="text" name="mate" id="mate">
+                  <div class="row">
+
+                    <div class="col-md-6">
+                      <p>Matematicas:</p>
+                    </div>
+
+                    <div class="col-md-4">
+                      <input class="form-control" type="number" name="mate" id="mate">
+                    </div>
+
+                  </div>
                 </div>
+
+
               </div>
+              <br>
               <div class="row">
+
                 <div class="col-md-6">
-                  <label for="">INGRESE LA CALIFICACION DE CIENCIAS SOCIALES</label>
-                  <input class="form-control"  type="text" name="cienS" id="cienS">
+                  <div class="row">
+
+                    <div class="col-md-6">
+                      Ciencias Sociales:
+                    </div>
+
+                    <div class="col-md-4">
+                      <input class="form-control" type="number" name="cienS" id="cienS">
+                    </div>
+
+                  </div>
                 </div>
+
                 <div class="col-md-6">
-                  <label for="">INGRESE LA CALIFICACION DE CIENCIAS NARUTALES</label>
-                  <input class="form-control"  type="text" name="cienN" id="cienN">
+                  <div class="row">
+
+                    <div class="col-md-6">
+                      Ciencias Naturales:
+                    </div>
+
+                    <div class="col-md-4">
+                      <input class="form-control" type="number" name="cienN" id="cienN">
+                    </div>
+
+                  </div>
+
                 </div>
+
               </div>
+              <br>
               <div class="row">
+
                 <div class="col-md-6">
-                  <label for="">INGRESE LA CALIFICACION DE INGLES</label>
-                  <input class="form-control" type="text" name="ingles" id="ingles">
+                  <div class="row">
+
+                    <div class="col-md-6">
+                      Ingles:
+                    </div>
+
+                    <div class="col-md-4">
+                      <input class="form-control" type="number" name="ingles" id="ingles">
+                    </div>
+
+
+                  </div>
                 </div>
+
               </div>
+
             </div>
 
           </div>
         </div>
         <div class="modal-footer">
+          <button id="_guardar" class="btn btn-primary">GUARDAR Y CONTINUAR NUEVO REGISTRO</button>
           <button type="button" class="btn btn-secondary" data-dismiss="modal">CERRAR</button>
-          <input class="btn btn-primary" type="submit" value="REGISTRAR">
         </div>
       </form>
     </div>

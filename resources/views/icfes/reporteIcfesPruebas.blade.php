78359e2fad2efd1aeb746059e3f28fc8dc80d4d7
Juan Sebastian Gomez Mezu <62127725+kingomez21@users.noreply.github.com>
2022-11-03 20:21:44 -0500

    Update reporteIcfesPruebas.blade.php

--- resources/views/icfes/reporteIcfesPruebas.blade.php
+++ resources/views/icfes/reporteIcfesPruebas.blade.php
@@ -634,7 +634,7 @@
             ingles === true
         ) {
             $.ajax({
-                url: '/registro_icfes/',
+                url: '/registro_icfes',
                 type: 'POST',
                 data: {
                     '_token': _formRegistro['_token'].value,

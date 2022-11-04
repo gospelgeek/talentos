129f2e3203af669ff19561b3d6a1640143048009
juliantello <95248642+juliantello@users.noreply.github.com>
2022-11-04 03:50:01 -0500

    Update detalles_sesiones.blade.php

--- resources/views/perfilEstudiante/Asistencias/Individuales/modal/detalles_sesiones.blade.php
+++ resources/views/perfilEstudiante/Asistencias/Individuales/modal/detalles_sesiones.blade.php
@@ -1,18 +1,34 @@
 <div class="modal" data-refresh="true"  tabindex="-1" id="modal_sesiones">
   <div class="modal-dialog modal-lg">
     <div class="modal-content">
-      <div id="recargar" class="modal-body">
+      <div class="modal-body">
         <button type="button" class="close" data-dismiss="modal">&times;</button>
         <h4 id="nombre" style="text-align:center;font-size : 30px;font-weight: bolder;"></h4>
+        <hr>
         <div  class="table-responsive">
         <table id="sesiones_tabla" class="table table-bordered table-striped">
           <caption id="mensaje" style="caption-side: top;text-align:center;"></caption>
           <thead>
-            <td>Sesion</td>
-            <td>Asistio</td>
+            <th>Sesion</th>
+            <th>Asistio</th>
           </thead>
-          <tbody id="sesiones">
-          </tbody>
+          <tfoot>
+            <th>Total Asistencias</th>
+            <th></th>
+          </tfoot>
+        </table>
+        <hr>
+        <h4 id="sesiones_adicionales" style='display:none;'>ASISTENCIAS EN OTROS GRUPOS</h4>
+        <table id="sesiones_tabla_2" class="table table-bordered table-striped" style='display:none'>
+          <thead>
+            <th>Sesion</th>
+            <th>Grupo</th>
+            <th>Asistio</th>
+          </thead>
+          <tfoot>
+            <th colspan="2">Total Asistencias</th>
+            <th></th>
+          </tfoot>
         </table>
       </div>
       </div>
@@ -21,6 +37,4 @@
       </div>
     </div>
   </div>
-</div>
-
-                        
+</div>         

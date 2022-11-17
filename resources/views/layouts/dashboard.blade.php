f9e573c9f9c02b13215b12b5a6988bd546539417
StevenDavila <72047032+StevenDavila@users.noreply.github.com>
2022-11-16 22:49:02 -0500

    Update dashboard.blade.php

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -223,7 +223,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
             <a class="dropdown-item" href="{{route('estudiantes_mayoria_edad')}}">Mayoria de edad</a>
             <a class="dropdown-item" href="{{route('descarga.certificado')}}">Certificado</a>
             <a class="dropdown-item" href="{{route('datos_pendientes')}}">Datos Pendientes</a>
-            <a class="dropdown-item" href="{{route('proceso_clasificacion')}}">Proceso de Clasificación</a>
+            <a class="dropdown-item" href="{{route('index_proceso_clasificacion')}}">Proceso de Clasificación</a>
           </div>
         </li>
         <li class="nav-item dropdown">
@@ -300,7 +300,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
                           <a  href="{{route('formalizacion')}}" class="dropdown-item">Formalizacion</a>
                           <a class="dropdown-item" href="{{route('descarga.certificado')}}">Certificado</a>
                           <a class="dropdown-item" href="{{route('datos_pendientes')}}">Datos Pendientes</a>
-                          <a class="dropdown-item" href="{{route('proceso_clasificacion')}}">Proceso de Clasificación</a>
+                          <a class="dropdown-item" href="{{route('index_proceso_clasificacion')}}">Proceso de Clasificación</a>
                         </div>
                       </li>
                       <li class="nav-item dropdown">

6d6a5428ec330892e84f7e9bc2773c091454cb29
StevenDavila <72047032+StevenDavila@users.noreply.github.com>
2022-11-19 19:49:31 -0500

    Create detalle_programa.blade.php

---
+++ resources/views/administrativo/procesoClasificacion/modal/detalle_programa.blade.php
@@ -0,0 +1,29 @@
+<div class="modal" data-refresh="true"  tabindex="-1" id="modal_detalle_programa">
+  <div class="modal-dialog modal-lg">    
+    <div class="modal-content">
+      <div class="modal-header">
+        <h4 class="modal-title pull-center" style="justify-content: center"><Strong>ESTUDIANTES CLASIFICADOS</strong></h4>
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+      </div>
+      <div class="modal-body">
+        <div  class="table-responsive">
+          <table id="detalle_programa" class="table table-bordered table-striped">
+            <thead>
+              <td>Nombres</td>
+              <td>Apellidos</td>
+              <td>Ponderacion Total</td>
+              <td>Ponderacion Areas</td>
+              <td>Promedio de Notas</td>
+              <td>Puesto</td>
+            </thead>
+          </table>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
